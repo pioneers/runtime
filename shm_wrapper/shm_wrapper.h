d29b5585afedc9de800ca9f3454e3f9ca54c8b68
Ben Liao <benliao1@berkeley.edu>
2020-05-28 01:29:53 -0700

    [SHM_WRAPPER] added synchronization between tests, included catalog sem lock on read/write, converted prints in shm_wrapper to logger messages
    Former-commit-id: 3425c5cee4c2f24090a50d39b04536d9b850a882

--- shm_wrapper/shm_wrapper.h
+++ shm_wrapper/shm_wrapper.h
@@ -10,6 +10,7 @@
 #include <unistd.h>                        //for standard symbolic constants
 #include <semaphore.h>                     //for semaphores
 #include <sys/mman.h>                      //for posix shared memory
+#include "../logger/logger.h"              //for logger (TODO: consider removing relative pathname in include)
 #include "../runtime_util/runtime_util.h"  //for runtime constants (TODO: consider removing relative pathname in include)
 
 //enumerated names for the two associated blocks per device
