b3088085582b62c824298406698525b255156aed
James Shi <james.shi@berkeley.edu>
2020-06-08 16:14:22 -0700

    basic zmq test

---
+++ zmq_comm/types.h
@@ -0,0 +1,24 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef struct one_s
+{
+    int a;
+    int b;
+} one_t;
+
+typedef struct two_s
+{
+    float a;
+    char b;
+    char c;
+} two_t;
+
+typedef struct three_s
+{
+    char a;
+    float b;
+    int c;
+} three_t;
+
+#endif
