3c15253191c070023d73980e2badd2f8dd5f09bd
Vincent Le <levincent06@berkeley.edu>
2020-06-13 11:39:20 -0700

    [DEV_HANDLER] Able to send Ping and await response - On device disconnect, spawn sender, receiver, relayer - Relayer claims interface, gets endpoints, sends ping - Ping tries to parse and verify SubscriptionResponse - Non-deterministic segfault on device connect

--- dev_handler/dev_handler.h
+++ dev_handler/dev_handler.h
@@ -12,7 +12,7 @@
 #define DEV_HANDLER_H
 
 #include <libusb.h>
-// #include <pthread.h>
+#include <pthread.h>
 #include "../runtime_util/runtime_util.h"
 // #include "../shm_wrapper/shm_wrapper.h"
 // #include "string.h" // strcmp
@@ -21,9 +21,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <signal.h> // Used to handle SIGTERM, SIGINT, SIGKILL
-// #include <time.h>   // For timestamps on device connects/disconnects
-
-#define MAX_PORTS 256
+#include <time.h>   // For timestamps on HeartBeatRequests
 
 /*
  * A struct defining a device's information for the lifetime of its connection
@@ -48,11 +46,6 @@ void init();
  * Frees data structures, stops libusb, and stops shared memory
  */
 void stop();
-
-/*
- * Catches Ctrl+C, calls stop(), and exits the program
- */
-void sigintHandler(int sig_num);
 
 /*******************************************
  *   FUNCTIONS TO TRACK AND UNTRACK DEVS   *
@@ -101,17 +94,34 @@ void poll_connected_devices();
 typedef struct msg_relay {
     libusb_device* dev;
     libusb_device_handle* handle;
-    pthread_t* sender;
-    pthread_t* receiver;
-    dev_id_t* dev_id;       // set by relayer once SubscriptionResponse is received
+    pthread_t sender;
+    pthread_t receiver;
+    pthread_t relayer;
+    uint8_t send_endpoint;
+    uint8_t receive_endpoint;
     uint8_t start;		    // set by relayer; a flag to tell reader and receiver to start work
+    dev_id_t dev_id;        // set by relayer once SubscriptionResponse is received
     uint8_t sent_hb_req;	// set by write: relay should expect a hb response
     uint8_t got_hb_req;	    // set by read: write should send a hb response
 } msg_relay_t;
 
-/* The max number of seconds to wait for a SubscriptionResponse or HeartBeatRequest
- * Waiting for this long will exit all threads (doing cleanup as necessary) */
-#define DEVICE_TIMEOUT 5;
+/* The maximum number of milliseconds to wait for a SubscriptionResponse or HeartBeatResponse
+ * Waiting for this long will exit all threads for that device (doing cleanup as necessary) */
+#define DEVICE_TIMEOUT 1000
+
+/* The number of milliseconds between each HeartBeatRequest sent to the device */
+#define HB_REQ_FREQ 500
+
+/*
+ * Opens threads for communication with a device
+ * Three threads will be opened:
+ *  relayer: Verifies device is lowcar and cancels threads when device disconnects/timesout
+ *  sender: Sends changed data in shared memory and periodically sends HeartBeatRequests
+ *  receiver: Receives data from the lowcar device and signals sender thread about HeartBeatRequests
+ *
+ * dev: The libusb_device to communicate with
+ */
+void communicate(libusb_device* dev);
 
 /*
  * Sends a Ping to the device and waits for a SubscriptionResponse
@@ -119,35 +129,41 @@ typedef struct msg_relay {
  * Connects the device to shared memory and signals the sender and receiver to start
  * Continuously checks if the device disconnected or HeartBeatRequest was sent without a response
  *      If so, it disconnects the device from shared memory, closes the device, and frees memory
+ * RELAY_CAST is to be casted as msg_relay_t
  */
-void relayer(msg_relay_t* relay);
+void* relayer(void* relay);
+
+/* Called by relayer to cancel threads and free RELAY */
+void relay_clean_up(msg_relay_t* relay);
 
 /*
  * Continuously reads from shared memory to serialize the necessary information
  * to send to the device.
  * Sets relay->sent_hb_req when a HeartBeatRequest is sent
  * Sends a HeartBeatResponse when relay->got_hb_req is set
+ * RELAY_CAST is to be casted as msg_relay_t
  */
-void sender(msg_relay_t* relay);
+void* sender(void* relay);
 
 /*
  * Continuously attempts to parse incoming data over serial and send to shared memory
  * Sets relay->got_hb_req upon receiving a HeartBeatRequest
+ * RELAY_CAST is to be casted as msg_relay_t
  */
-void receiver(msg_relay_t* relay);
+void* receiver(void* relay);
 
 /*******************************************
  *             DEVICE POLLING              *
  *******************************************/
 /*
- * Asynchronously sends a PING message to a device to check if it's a lowcar device
- * dev: The device to send a PING to
+ * Synchronously sends a PING message to a device to check if it's a lowcar device
+ * relay: Struct holding device, handle, and endpoint fields
+ * return: 0 upon successfully receiving a SubscriptionResponse and setting relay->dev_id
+ *      1 if Ping message couldn't be serialized
+ *      2 if Ping message couldn't be sent or SubscriptionResponse wasn't received
+ *      3 if received message has incorrect checksum
+ *      4 if received message is not a SubscriptionResponse
  */
-void ping(libusb_device* dev);
+int ping(msg_relay_t* relay);
 
-/*
- * The callback function for sending a PING message to a device
- * Prints info about the status of the PING transfer
- */
-void ping_callback(struct libusb_transfer* transfer);
 #endif
