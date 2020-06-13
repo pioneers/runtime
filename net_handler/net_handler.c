f11e5d067a4cc4bd8117be7d5d79bb55ca982060
Ben Liao <benliao1@berkeley.edu>
2020-06-13 14:49:00 -0700

    [NET_HANDLER] initial commit

---
+++ net_handler/net_handler.c
@@ -0,0 +1,26 @@
+#include "net_handler.h"
+
+//IP addresses of Dawn and Shepherd and port numbers
+
+void *udp_send_thread (void *args)
+{
+	
+}
+
+void *udp_recv_thread (void *args)
+{
+	
+}
+
+int main ()
+{
+	//set up
+	
+	while (1) {
+		//something something open sockets and wait for connections
+		//on connection spawn threads to handle communication
+		//handle dropped connections and opening new sockets in this loop too
+	}
+	
+	return 0;
+}
