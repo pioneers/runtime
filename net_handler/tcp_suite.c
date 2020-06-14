0f17e48a1211fe47721773bf4b28b869a25f5834
Ben Liao <benliao1@berkeley.edu>
2020-06-14 01:01:41 -0700

    [NET_HANDLER] skeleton code for tcp and udp send and recv threads

--- net_handler/tcp_suite.c
+++ net_handler/tcp_suite.c
@@ -1,60 +1,141 @@
 #include "tcp_suite.h"
 
+uint16_t ports[2][2] = { {8000, 8001} , {8100, 8101} }; //hard-coded port numbers
+pthread_t td_ids[2][2];
+td_arg_t global_td_args[2];
+
+//structure of argument into a pair of tcp threads
+typedef struct tcp_arg = {
+	uint8_t challenge_wip;
+	uint16_t send_port;
+	uint16_t recv_port;
+	uint8_t stop;
+} tcp_arg_t;
+
+static void error (char *msg)
+{
+	perror(msg);
+	log_runtime(ERROR, msg); //send the message to the logger
+	exit(1);
+}
+
 //send LOG messages to shepherd and dawn
 void *tcp_send_thread (void *args)
 {	
+	tcp_arg_t *tcp_args = (tcp_arg_t *)args; //use this to communicate between the send and recv threads
+	
 	char *buf = (char *) malloc(sizeof(char) * log_msg_maxlen);
 	log_payload_t log_payload;
 	text_payload_t text_payload;
 	log_payload.msg = LOG;
-	text_payload.msg = CODE_UPLOAD_REQUEST;
+	text_payload.msg = CHALLENGE_DATA;
 	
 	//open log file and go to the end
 	FILE *fp;
 	fp = fopen("../logger/runtime_log.log", "r");
 	fseek(fp, 0, SEEK_END);
 	
-	//TODO: add condition variable to stop thread
-	while (1) {
-		log_payload.num_logs = 0;
-		while (log_payload.num_logs < MAX_NUM_LOGS) {
-			if (fgets(buf, LOG_MSG_MAXLEN, fp) != NULL) {
-				strcpy(log_payload.logs[log_payload.num_logs], (const char *) buf);
-				log_payload.num_logs++;
-			} else {
-				break;
-			}
-			usleep(1000); //wait a bit so we aren't going as fast as possible
+	while (!(tcp_args->stop)) {
+		if (fgets(buf, LOG_MSG_MAXLEN, fp) != NULL) { //if there's a log message to send
+			log_payload.num_logs = 0;
+			do {
+				strcpy(log_payload.logs[log_payload.num_logs], (const char *)buf);
+				log_payload_num_logs++;
+			} while (fgets(buf, LOG_MSG_MAXLEN, fp) != NULL); //while there's still stuff to read, get it all
+			//TODO: package data and send over TCP socket
 		}
-		//TODO: package data and send over TCP socket
+		if (tcp_args->challenge_wip) {
+			if (/* TODO: check to see if coding challenge results are in */) {
+				/* TODO: read in, package, encode, send over TCP port */
+				tcp_args->challenge_wip = 0; //coding challenge not wip any more
+			}
+		}
+		usleep(1000); //sleep a bit so we're not looping as fast as possible
 	}
+	return NULL;
 }
 
 //receive RUN_MODE, CODE_UPLOAD_REQUEST over TCP
 void *tcp_recv_thread (void *args)
 {
-	text_payload_t payload;
+	tcp_arg_t *tcp_args = (tcp_arg_t *)args; //use this to communicate between the send and recv threads
 	
-	while (1) {
+	text_payload_t payload;
+	payload.msg = NOP;
+	
+	while (!(tcp_args->stop)) {
+		// TODO: receive a message from the port and load into payload (do NOT block here!)
 		
+		//handle the received packet
+		if (payload.msg == RUN_MODE) {
+			if (!strcmp(payload.text, "IDLE")) {
+				log_runtime(DEBUG, "entering IDLE mode");
+				robot_desc_write(RUN_MODE, IDLE);
+			} else if (!strcmp(payload.text, "AUTO")) {
+				log_runtime(DEBUG, "entering AUTO mode");
+				robot_desc_write(RUN_MODE, AUTO);
+			} else if (!strcmp(payload.text, "TELEOP")) {
+				log_runtime(DEBUG, "entering TELEOP mode");
+				robot_desc_write(RUN_MODE, TELEOP);
+			} else if (!strcmp(payload.text, "CHALLENGE")) {
+				log_runtime(DEBUG, "entering CHALLENGE mode; running coding challenges!");
+				robot_desc_write(RUN_MODE, CHALLENGE); //TODO: won't compile as of now, need to check this
+				tcp_args->challenge_wip = 1; //tell send thread to start waiting for coding challenge to finish
+			} else {
+				log_runtime(WARN, "requested robot to enter unknown robot mode");
+			}
+		} else if (payload.msg == CHALLENGE_DATA) {
+			//TODO: take payload.text and put it somewhere for the executor to use when running challenges
+		} else if (payload.msg != NOP) {
+			log_runtime(WARN, "unknown packet type received on TCP");
+		}
+		payload.msg = NOP; //reset this
+		usleep(1000); //sleep a bit so we're not looping as fast as possible
 	}
+	free(tcp_args); //recv thread is responsible for freeing the argument
+	return NULL;
 }
 
-
-
-//start a trio of threads managing a TCP connection
-void start_tcp_suite (char *port)
+//start the threads managing a TCP connection
+void start_tcp_suite (endpoint_t endpoint)
 {
-	//load argument and prepare to start; create any synchronization variables necessary
-	//create sender thread
-	//create receiver thread
-	//create manage thread
+	tcp_arg_t td_args = (tcp_arg_t *) malloc(sizeof(tcp_arg_t));
+	td_args->challenge_wip = 0;
+	td_args->send_port = ports[endpoint][0];
+	td_args->recv_port = ports[endpoint][1];
+	td_args->stop = 0;
+	char msg[64];     //for error messages
+	
+	//create send thread
+	if (pthread_create(&td_ids[endpoint][0], NULL, tcp_send_thread, td_args) != 0) {
+		sprintf(msg, "failed to create TCP send thread with endpoint %d", endpoint);
+		error(msg);
+	}
+	
+	//create recv thread
+	if (pthread_create(&td_ids[endpoint][1], NULL, tcp_recv_thread, td_args) != 0) {
+		sprintf(msg, "failed to create TCP recv thread with endpoint %d", endpoint);
+		error(msg);
+	}
+	
+	//save this for later telling the threads to stop
+	global_td_args[endpoint] = td_args; 
 }
 
-//stop the trio of threads managing the TCP connection
-void stop_tcp_suite (char *port)
+//stop the threads managing the TCP connection
+void stop_tcp_suite (endpoint_t endpoint)
 {
-	//signal to relay thread that it needs to stop the other threads
-	//join with all of them (or until timeout)
-	//return
+	global_td_args[endpoint].stop = 1;
+	
+	//join with send thread
+	if (pthread_join(td_ids[endpoint][0], NULL) != 0) {
+		sprintf(msg, "failed to join TCP send thread with endpoint %d", endpoint);
+		error(msg);
+	}
+	
+	//join with recv thread
+	if (pthread_join(td_ids[endpoint][1], NULL) != 0) {
+		sprintf(msg, "failed to join TCP recv thread with endpoint %d", endpoint);
+		error(msg);
+	}
 }
---
+++ net_handler/udp_suite.c
@@ -0,0 +1,144 @@
+#include "udp_suite.h"
+
+uint16_t ports[2] = { 8200, 8201 }; //hard-coded port numbers
+pthread_t td_ids[2];
+td_arg_t global_td_args;
+
+//structure of argument into a pair of udp threads
+typedef struct udp_arg = {
+	uint16_t send_port;
+	uint16_t recv_port;
+	uint8_t stop;
+} udp_arg_t;
+
+static void error (char *msg)
+{
+	perror(msg);
+	log_runtime(ERROR, msg); //send the message to the logger
+	exit(1);
+}
+
+//send DEVICE_DATA messages to Dawn
+void *udp_send_thread (void *args)
+{	
+	udp_arg_t *udp_args = (udp_arg_t *)args; //use this to communicate between the send and recv threads
+	dev_data_t dev_data;
+	dev_data.msg = DEVICE_DATA;
+	
+	int valid_dev_ixs[MAX_DEVICES];
+	dev_id_t dev_ids[MAX_DEVICES];
+	uint32_t catalog, params_to_read;
+	uint8_t curr_num_params;         //number of params for device currently being operated on
+	int j;
+	
+	//TODO: figure out what happens if a device disconnects in the middle of pulling information
+	while (!(udp_args->stop)) {
+		//get information
+		get_catalog(&catalog);
+		get_device_identifiers(dev_ids);
+		
+		//calculate num_devices, get valid device indices
+		dev_data.num_devices = 0;
+		for (int i = 0, j = 0; i < MAX_DEVICES; i++) {
+			if (catalog & (1 << i)) {
+				dev_data.num_devices++;
+				valid_dev_ixs[j++] = i;
+			}
+		}
+		
+		//populate dev_data.device_ids
+		for (int i = 0; i < dev_data.num_devices; i++) {
+			dev_data.device_ids[i] = dev_ids[valid_dev_ixs[i]];
+		}
+		
+		//populate dev_data.device_params
+		dev_data.device_params = (param_val_t **) malloc(sizeof(param_val_t *) * dev_data.num_devices);
+		for (int i = 0; i < dev_data.num_devices; i++) {
+			curr_num_params = (get_device(dev_data.device_ids[i].type))->num_params;
+			dev_data.device_params[i] = (param_val_t *) malloc(sizeof(param_val_t) * curr_num_params);
+			params_to_read = 0;
+			for (int k = 0; k < curr_num_params; k++) {
+				params_to_read |= (1 << k); //turn on the bit for every valid param
+			}
+			device_read(valid_dev_ixs[i], NET_HANDLER, DATA, params_to_read, &(dev_data.device_params[i])); //pull info
+		}
+		
+		//TODO: package data and send over UDP socket
+		
+		//free everything
+		for (int i = 0; i < dev_data.num_devices; i++) {
+			free(dev_data.device_params[i]);
+		}
+		free(dev_data.device_params);
+		
+		usleep(1000); //sleep a bit so we're not looping as fast as possible
+	}
+	return NULL;
+}
+
+//receive GAMEPAD_STATE messages from DAWN
+void *udp_recv_thread (void *args)
+{
+	udp_arg_t *udp_args = (udp_arg_t *)args; //use this to communicate between the send and recv threads
+	
+	gp_state_t gp_state;
+	gp_state = NOP;
+	
+	while (!(udp_args->stop)) {
+		// TODO: receive a message from the port and load into gp_state (do NOT block here!)
+		
+		//handle the received packet
+		if (gp_state.msg == GAMEPAD_STATE) {
+			gamepad_write(gp_state.buttons, gp_state.joystick_values);
+		} else if (gp_state.msg != NOP) {
+			log_runtime(WARN, "unknown packet type received on UDP");
+		}
+		gp_state.msg = NOP;
+		usleep(1000); //sleep a bit so we're not looping as fast as possible
+	}
+	free(udp_args); //recv thread is responsible for freeing the argument
+	return NULL;
+}
+
+//start the threads managing a UDP connection
+void start_udp_suite ()
+{
+	udp_arg_t td_args = (udp_arg_t *) malloc(sizeof(udp_arg_t));
+	td_args->send_port = ports[0];
+	td_args->recv_port = ports[1];
+	td_args->stop = 0;
+	char msg[64];     //for error messages
+	
+	//create send thread
+	if (pthread_create(&td_ids[0], NULL, udp_send_thread, td_args) != 0) {
+		sprintf(msg, "failed to create UDP send thread with Dawn");
+		error(msg);
+	}
+	
+	//create recv thread
+	if (pthread_create(&td_ids[1], NULL, udp_recv_thread, td_args) != 0) {
+		sprintf(msg, "failed to create UDP recv thread with Dawn");
+		error(msg);
+	}
+	
+	//save this for later telling the threads to stop
+	global_td_args = td_args;
+}
+
+//stop the threads managing the UDP connection
+void stop_udp_suite ()
+{
+	global_td_args.stop = 1;
+	
+	//join with send thread
+	if (pthread_join(td_ids[0], NULL) != 0) {
+		sprintf(msg, "failed to join UDP send thread with Dawn");
+		error(msg);
+	}
+	
+	//join with recv thread
+	if (pthread_join(td_ids[endpoint][1], NULL) != 0) {
+		sprintf(msg, "failed to join UDP recv thread with Dawn");
+		error(msg);
+	}
+}
