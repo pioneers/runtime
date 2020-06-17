f538e5e84349043ea3c0bd48ee6b6aa850c2190d
Ashwin Vangipuram <ashwinvangipuram@gmail.com>
2020-06-17 01:10:07 -0400

    [NET_HANDLER] Changed Makefile to work for compiled CZMQ

--- net_handler/test_czmq.c
+++ net_handler/test_czmq.c
@@ -1,12 +1,12 @@
 #include <czmq.h>
-#include <zmq.h>
 
 int main (void)
 {
     int major, minor, patch;
     zmq_version (&major, &minor, &patch);
     printf ("Current 0MQ version is %d.%d.%d\n", major, minor, patch);
-    zsock_t *radio = zsock_new_radio ("inproc://zframe-test-radio");
+    printf("%d\n", zmq_has("draft"));
+    zsock_t *radio = zsock_new_radio("ipc://zframe-test-radio");
     assert(radio);
     zsock_destroy(&radio);
     return 0;
