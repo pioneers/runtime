0a605f81bc96be6488f9266d1176f48a183a4ff0
Vincent Le <levincent06@berkeley.edu>
2020-06-21 17:52:17 -0700

    [DEV_HANDLER] Added fake device test files
    Former-commit-id: 24e892f61316e5bf0d70f2376421c2b23990ae19

---
+++ dev_handler/tests/fake_device.c
@@ -0,0 +1,183 @@
+#include "fake_device.h"
+
+/* Struct shared between the three threads so they can communicate with each other
+ * relayer thread acts as the "control center" and connects/disconnects to shared memory
+ *  relayer thread also frees memory when device is disconnected
+ */
+typedef struct msg_relay {
+    pthread_t sender;
+    pthread_t receiver;
+    pthread_t relayer;
+    FILE* read_file;        // File to read from
+    FILE* write_file;       // File to write to
+    uint8_t start;		    // set by relayer: A flag to tell reader and receiver to start work when set to 1
+    dev_id_t dev_id;        // set by relayer once SubscriptionResponse is received
+    uint8_t got_write;
+    uint8_t got_read;
+    uint64_t sent_hb_req;	// set by sender: The TIME that the last HeartBeatRequest was sent that is not yet resolved. 0 if there is no pending request
+    uint8_t got_hb_req;	// set by receiver: A flag to tell sender to send a HeartBeatResponse
+} msg_relay_t;
+
+/* Returns the number of milliseconds since the Unix Epoch */
+int64_t millis() {
+	struct timeval time; // Holds the current time in seconds + microsecondsx
+	gettimeofday(&time, NULL);
+	int64_t s1 = (int64_t)(time.tv_sec) * 1000; // Convert seconds to milliseconds
+	int64_t s2 = (time.tv_usec / 1000);			// Convert microseconds to milliseconds
+	return s1 + s2;
+}
+
+
+/*
+ * Attempts to read a message in to_device.txt and parses it into a message_t
+ * msg: The message_t* to be populated with the parsed data (if successful)
+ * return:  0 if successfully parsed
+ *          1 if no message
+ *          2 if broken message
+ *          3 if incorrect checksum
+ */
+int receive_message(message_t* relay, message_t* msg) {
+    uint8_t last_byte_read;
+    // Try to read the delimiter
+    size_t num_bytes_read = fread(&last_byte_read, sizeof(uint8_t), 1, relay->read_file);
+    if (num_bytes_read == 0) {
+        return 1;
+    } else if (last_byte_read != 0) {
+        printf("FATAL: Read something besides the delimiter\n");
+        return 2;
+    }
+    // Try to read the length of the cobs encoded message
+    num_bytes_read = fread(&last_byte_read, sizeof(uint8_t), 1, relay->read_file);
+    if (num_bytes_read == 0) {
+        return 1;
+    }
+    int cobs_len = last_byte_read;
+    // Allocate buffer to read message into
+    uint8_t data[cobs_len];
+    // Read the message
+    num_bytes_read = fread(data, sizeof(uint8_t), cobs_len, relay->read_file);
+    if (num_bytes_read != cobs_len) {
+        printf("FATAL: Couldn't read the full message\n");
+        return 2;
+    }
+    // Parse the message
+    if (parse_message(data, msg) != 0) {
+        printf("FATAL: Incorrect checksum\n");
+        return 3;
+    }
+    return 0;
+}
+
+/*
+ * Attempts to serialize and cobs encode a message into to_dev_handler.txt
+ * msg: Pointer to the message_t to be serialized, cobs-encoded, and written to the .txt file
+ */
+void send_message(message_t* relay, message_t* msg) {
+    // Allocate memory for data buffer
+    int len = calc_max_cobs_msg_length(msg);
+    uint8_t data[len];
+    // Fill data buffer with serialized, then encoded message
+    len = message_to_bytes(msg, data, len);
+    // Write the data buffer to the file
+    fwrite(data, sizeof(uint8_t), len, relay->write_file);
+}
+
+/*
+ *  Thread to continuously send messages to to_dev_handler.txt
+ *  Sends DeviceData, HeartBeatRequest, HeartBeatResponse, and Log
+ */
+void* sender(void* relay_cast) {
+    // Sleep until relay->start == 1, which is true when relayer sends a SubscriptionResponse
+	while (1) {
+		if (relay->start == 1) {
+			break;
+		}
+		pthread_testcancel(); // Cancellation point. Relayer may cancel this thread if device timesout
+	}
+    // Allocate a message to be sent
+    message_t* msg = make_empty(MAX_PAYLOAD_SIZE);
+    // Continuously send messages as needed
+    while (1) {
+        if (relay->got_hb_req) {
+            // Send a HeartBeatResponse
+            message_t* msg = make_heartbeat_response(0);
+            send_message(relay, msg);
+            destroy_message(msg);
+            relay->got_hb_req = 0;
+        } else if ((millis() - relay->sent_hb_req) > HB_REQ_FREQ) {
+            // If it's been been too long since the last HeartBeatRequest, send another one
+            message_t* msg = make_heartbeat_request(0);
+            send_message(relay, msg);
+            destroy_message(msg);
+            relay->sent_hb_req = millis();
+        } else if (relay->got_write) {
+            // TODO: Send DeviceData
+            relay->got_write = 0;
+        } else if (relay->got_read) {
+            // TODO: Send DeviceData
+            relay->got_read = 0;
+        }
+        pthread_testcancel();
+    }
+}
+
+/*
+ *  Thread to continuously read messages from to_device.txt and take action based on the message
+ *  Receives DeviceRead, DeviceWrite, HeartBeatRequest, HeartBeatResponse
+ */
+void* receiver(void* relay_cast) {
+    // Sleep until relay->start == 1, which is true when relayer sends a SubscriptionResponse
+	while (1) {
+		if (relay->start == 1) {
+			break;
+		}
+		pthread_testcancel(); // Cancellation point. Relayer may cancel this thread if device timesout
+	}
+    // Allocate message
+    message_t* msg = make_empty(MAX_PAYLOAD_SIZE);
+    // Continuously read messages and take appropriate action
+    while (1) {
+        if (receive_message(relay, msg) != 0) {
+            continue;
+        }
+        if (msg->message_id = DeviceRead) {
+            // TODO: Send DeviceData
+        } else if (msg->message_id == DeviceWrite) {
+            // TODO: Change vals and send DeviceData
+        } else if (msg->message_id == HeartBeatRequest) {
+            relay->got_hb_req = 1;
+        } else if (msg->message_id == HeartBeatResponse) {
+            relay->sent_hb_req = 0;
+        } else {
+            printf("FATAL: Got message of unknown type\n");
+        }
+        pthread_testcancel();
+    }
+}
+
+/*
+ * Thread to make sure sender and receiver and synced
+ */
+void* relayer(void* relay_cast) {
+
+}
+
+void init() {
+    // Allocate relay object
+    msg_relay_t* relay = malloc(sizeof(msg_relay_t));
+    // Open the file to read from
+    relay->read_file = fopen(TO_DEVICE, "r");
+    // Open the file to write to
+    relay->write_file = fopen(TO_DEV_HANDLER, "w");
+    // Don't start sending and receiving just yet
+    relay->start = 0;
+    // Set the device id
+    relay->dev_id.type = TYPE;
+    relay->dev_id.year = YEAR;
+    relay->dev_id.uid = UID;
+    // Open threads for sender, receiver, and relayer
+	pthread_create(&relay->sender, NULL, sender, relay);
+	pthread_create(&relay->receiver, NULL, receiver, relay);
+	pthread_create(&relay->relayer, NULL, relayer, relay);
+    printf("INFO: Fake device powered on\n");
+}
---
+++ dev_handler/tests/fake_device.h
@@ -0,0 +1,31 @@
+/**
+ *  A fake device that reads incoming data from to_device.txt and sends data to to_dev_handler.txt
+ *  Those two txt files will act as data streams containing encoded data using the protocol as the dev handler
+ *  would use when communcating with a normal USB device
+ */
+
+#include <pthread.h>
+#include "../../runtime_util/runtime_util.h"
+#include "../message.h"
+#include <stdio.h> // Print
+#include <stdlib.h>
+#include <stdint.h> // ints with specified sizes (uint8_t, uint16_t, etc.)
+#include <time.h>   // For timestamps on HeartBeatRequests
+#include <unistd.h> // sleep(int seconds)
+
+// The file to read from
+#define TO_DEVICE "to_device.txt"
+
+// The file to write to
+#define TO_DEV_HANDLER "to_dev_handler.txt"
+
+// The fake device's ID numbers
+#define TYPE 0x0000             // 16-bit
+#define YEAR 0x01               // 8-bit
+#define UID 0x0123456789ABCDEF  // 64-bit
+
+// Interval (in milliseconds) at which we send a HeartBeatRequest
+#define HB_REQ_FREQ 2000
+
+// "Power on" the device
+void init();
