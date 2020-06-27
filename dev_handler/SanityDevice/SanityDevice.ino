3a5b93e90298c75203c2c5eace3c1e19f48ce685
Vincent Le <levincent06@berkeley.edu>
2020-06-26 21:19:12 -0700

    [DEV_HANDLER] SanityDevice better blinks - Fast blinks on good reads - Slow blinks on bad reads

--- dev_handler/SanityDevice/SanityDevice.ino
+++ dev_handler/SanityDevice/SanityDevice.ino
@@ -2,18 +2,46 @@
  * A very simple device to test that libusb works
  */
 #include <Arduino.h>
+#include "StatusLED.h"
 
-#define LED_PIN 13
+StatusLED *led; //declare the device
 
-/* Set the LED to output */
 void setup() {
     pinMode(LED_PIN, OUTPUT);
+    led = new StatusLED();
 }
 
 /* Every other second, toggle the LED */
 void loop() {
-    digitalWrite(LED_PIN, LOW);
-    delay(1000);
-    digitalWrite(LED_PIN, HIGH);
-    delay(1000);
+    read_message();
+}
+
+void read_message ()
+{
+	//if nothing to read
+	if (!Serial.available()) {
+        return;
+	}
+
+	//find the start of packet
+	int last_byte_read = -1;
+	while (Serial.available()) {
+        led->fast_blink(1); // If found something, blink fast
+		last_byte_read = Serial.read();
+		if (last_byte_read == 0) {
+            led->fast_blink(5); // If found the start of packet, blink fast 5 times
+			break;
+		}
+	}
+
+	if (last_byte_read != 0) { //no start of packet found
+        led->slow_blink(3);
+		return;
+	}
+	if (Serial.available() == 0 || Serial.peek() == 0) { //no packet length found
+        led->slow_blink(10);
+		return;
+	}
+    // We got a message
+    led->fast_blink(20);
 }
