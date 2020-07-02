7e804fd5921eb2a220422f30d59adc5deaffab7b
Ashwin Vangipuram <ashwinvangipuram@gmail.com>
2020-07-02 05:19:29 -0400

    [NET_HANDLER] Working net_handler with both TCP and UDP threds working. Fixed UDP thread shutdown to be safe

--- net_handler/tcp_conn.h
+++ net_handler/tcp_conn.h
@@ -9,12 +9,12 @@
  * Arguments:
  *    - int connfd: connection socket descriptor on which there is the established connection with Dawn
  */
-void start_tcp_conn (robot_desc_val_t client, int connfd, int send_logs);
+void start_tcp_conn (robot_desc_field_t client, int connfd, int send_logs);
 
 /*
  * Stops the TCP connection control thread cleanly. May block briefly to allow
  * main control thread to finish what it's doing. Should be called right before the net_handler main loop terminates.
  */
-void stop_tcp_conn ();
+void stop_tcp_conn (robot_desc_field_t client);
 
 #endif
