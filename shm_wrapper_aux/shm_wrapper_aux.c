afeec7bfbde5493a341558856b60a19c6ec3eddd
Ben Liao <benliao1@berkeley.edu>
2020-07-14 16:17:40 -0700

    [SHM][AUX SHM] Implementing requested changes - Check for log level done before variable argument list expansion - Added strerror() printing to shm and aux shm system calls

--- shm_wrapper_aux/shm_wrapper_aux.c
+++ shm_wrapper_aux/shm_wrapper_aux.c
@@ -170,50 +170,50 @@ void shm_aux_init (process_t process)
 		
 		//mutual exclusion semaphore, gp_sem with initial value = 0
 		if ((gp_sem = sem_open(GP_MUTEX_NAME, O_CREAT, 0660, 0)) == SEM_FAILED) {
-			log_printf(FATAL, "sem_open: gamepad_mutex@net_handler");
+			log_printf(FATAL, "sem_open: gamepad_mutex@net_handler: %s", strerror(errno));
 			exit(1);
 		}
 		gp_val = 0;
 		
 		//mutual exclusion semaphore, rd_sem with initial value = 1
 		if ((rd_sem = sem_open(RD_MUTEX_NAME, O_CREAT, 0660, 1)) == SEM_FAILED) {
-			log_printf(FATAL, "robot_desc_mutex@net_handler");
+			log_printf(FATAL, "robot_desc_mutex@net_handler: %s", strerror(errno));
 			exit(1);
 		}
 		rd_val = 1;
 		
 		//create gamepad shm block
 		if ((fd_shm = shm_open(GPAD_SHM_NAME, O_RDWR | O_CREAT, 0660)) == -1) {
-			log_printf(FATAL, "shm_open gamepad_shm: @net_handler");
+			log_printf(FATAL, "shm_open gamepad_shm: @net_handler: %s", strerror(errno));
 			exit(1);
 		}
 		if (ftruncate(fd_shm, sizeof(gamepad_t)) == -1) {
-			log_printf(FATAL, "ftruncate gamepad_shm: @net_handler");
+			log_printf(FATAL, "ftruncate gamepad_shm: @net_handler: %s", strerror(errno));
 			exit(1);
 		}
 		if ((gp_ptr = mmap(NULL, sizeof(gamepad_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd_shm, 0)) == MAP_FAILED) {
-			log_printf(FATAL, "mmap gamepad_shm @net_handler");
+			log_printf(FATAL, "mmap gamepad_shm @net_handler: %s", strerror(errno));
 			exit(1);
 		}
 		if (close(fd_shm) == -1) {
-			log_printf(ERROR, "close gamepad_shm: @net_handler");
+			log_printf(ERROR, "close gamepad_shm: @net_handler: %s", strerror(errno));
 		}
 		
 		//create robot description shm block
 		if ((fd_shm = shm_open(ROBOT_DESC_SHM_NAME, O_RDWR | O_CREAT, 0660)) == -1) {
-			log_printf(FATAL, "shm_open robot_desc_shm: @net_handler");
+			log_printf(FATAL, "shm_open robot_desc_shm: @net_handler: %s", strerror(errno));
 			exit(1);
 		}
 		if (ftruncate(fd_shm, sizeof(robot_desc_t)) == -1) {
-			log_printf(FATAL, "ftruncate robot_desc_shm: @net_handler");
+			log_printf(FATAL, "ftruncate robot_desc_shm: @net_handler: %s", strerror(errno));
 			exit(1);
 		}
 		if ((rd_ptr = mmap(NULL, sizeof(robot_desc_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd_shm, 0)) == MAP_FAILED) {
-			log_printf(FATAL, "mmap robot_desc_shm @net_handler");
+			log_printf(FATAL, "mmap robot_desc_shm @net_handler: %s", strerror(errno));
 			exit(1);
 		}
 		if (close(fd_shm) == -1) {
-			log_printf(ERROR, "close robot_desc_shm: @net_handler");
+			log_printf(ERROR, "close robot_desc_shm: @net_handler: %s", strerror(errno));
 		}
 		
 		//initialize everything
@@ -232,13 +232,13 @@ void shm_aux_init (process_t process)
 	} else {
 		//mutual exclusion semaphore, gamepad_sem
 		if ((gp_sem = sem_open(GP_MUTEX_NAME, 0, 0, 0)) == SEM_FAILED) {
-			log_printf(FATAL, "sem_open: gamepad_mutex@client");
+			log_printf(FATAL, "sem_open: gamepad_mutex@client: %s", strerror(errno));
 			exit(1);
 		}
 		
 		//mutual exclusion semaphore, rd_sem
 		if ((rd_sem = sem_open(RD_MUTEX_NAME, 0, 0, 0)) == SEM_FAILED) {
-			log_printf(FATAL, "sem_open: robot_desc_mutex@client");
+			log_printf(FATAL, "sem_open: robot_desc_mutex@client: %s", strerror(errno));
 			exit(1);
 		}
 		
@@ -247,28 +247,28 @@ void shm_aux_init (process_t process)
 		
 		//open gamepad shm block and map to client process virtual memory
 		if ((fd_shm = shm_open(GPAD_SHM_NAME, O_RDWR, 0)) == -1) { //no O_CREAT
-			log_printf(FATAL, "shm_open gamepad_shm: @client");
+			log_printf(FATAL, "shm_open gamepad_shm: @client: %s", strerror(errno));
 			exit(1);
 		}
 		if ((gp_ptr = mmap(NULL, sizeof(gamepad_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd_shm, 0)) == MAP_FAILED) {
-			log_printf(FATAL, "mmap gamepad_shm: @net_handler");
+			log_printf(FATAL, "mmap gamepad_shm: @net_handler: %s", strerror(errno));
 			exit(1);
 		}
 		if (close(fd_shm) == -1) {
-			log_printf(ERROR, "close gamepad_shm: @client");
+			log_printf(ERROR, "close gamepad_shm: @client: %s", strerror(errno));
 		}
 		
 		//open robot desc shm block and map to client process virtual memory
 		if ((fd_shm = shm_open(ROBOT_DESC_SHM_NAME, O_RDWR, 0)) == -1) { //no O_CREAT
-			log_printf(FATAL, "shm_open robot_desc_shm: @client");
+			log_printf(FATAL, "shm_open robot_desc_shm: @client: %s", strerror(errno));
 			exit(1);
 		}
 		if ((rd_ptr = mmap(NULL, sizeof(robot_desc_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd_shm, 0)) == MAP_FAILED) {
-			log_printf(FATAL, "mmap robot_desc_shm: @client");
+			log_printf(FATAL, "mmap robot_desc_shm: @client: %s", strerror(errno));
 			exit(1);
 		}
 		if (close(fd_shm) == -1) {
-			log_printf(ERROR, "close robot_desc_shm: @client");
+			log_printf(ERROR, "close robot_desc_shm: @client: %s", strerror(errno));
 		}
 		
 		//release gp_sem
@@ -288,10 +288,10 @@ void shm_aux_stop (process_t process)
 {	
 	//unmap the gamepad and robot desc shm blocks
 	if (munmap(gp_ptr, sizeof(gamepad_t)) == -1) {
-		(process == NET_HANDLER) ? log_printf(ERROR, "munmap gamepad_shm: @net_handler") : log_printf(ERROR, "munmap gamepad_shm: @client");
+		(process == NET_HANDLER) ? log_printf(ERROR, "munmap gamepad_shm: @net_handler: %s", strerror(errno)) : log_printf(ERROR, "munmap gamepad_shm: @client: %s", strerror(errno));
 	}
 	if (munmap(rd_ptr, sizeof(robot_desc_t)) == -1) {
-		(process == NET_HANDLER) ? log_printf(ERROR, "munmap robot_desc_shm: @net_handler") : log_printf(ERROR, "munmap robot_desc_shm: @client");
+		(process == NET_HANDLER) ? log_printf(ERROR, "munmap robot_desc_shm: @net_handler: %s", strerror(errno)) : log_printf(ERROR, "munmap robot_desc_shm: @client: %s", strerror(errno));
 	}
 	
 	//close both semaphores
@@ -307,18 +307,18 @@ void shm_aux_stop (process_t process)
 	if (process == NET_HANDLER) {
 		//unlink shared memory blocks
 		if (shm_unlink(GPAD_SHM_NAME) == -1) {
-			log_printf(ERROR, "shm_unlink gamepad_shm @net_handler");
+			log_printf(ERROR, "shm_unlink gamepad_shm @net_handler: %s", strerror(errno));
 		}
 		if (shm_unlink(ROBOT_DESC_SHM_NAME) == -1) {
-			log_printf(ERROR, "shm_unlink robot_desc_shm @net_handler");
+			log_printf(ERROR, "shm_unlink robot_desc_shm @net_handler: %s", strerror(errno));
 		}
 		
 		//unlink semaphores
 		if (sem_unlink(GP_MUTEX_NAME) == -1) {
-			log_printf(ERROR, "sem_unlink gamepad_mutex @net_handler");
+			log_printf(ERROR, "sem_unlink gamepad_mutex @net_handler: %s", strerror(errno));
 		}
 		if (sem_unlink(RD_MUTEX_NAME) == -1) {
-			log_printf(ERROR, "sem_unlink robot_desc_mutex @net_handler");
+			log_printf(ERROR, "sem_unlink robot_desc_mutex @net_handler: %s", strerror(errno));
 		}
 	}
 }
