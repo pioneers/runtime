b6a177a587d23e5f3620191c477fe75beb2632dd
James Shi <james.shi@berkeley.edu>
2020-07-18 14:01:21 -0700

    [SYSTEMD] Added test scripts

---
+++ systemd-stuff/script3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import time
+
+end = 7
+
+print("Start script3.py:", time.time())
+for i in range(end):
+    print(i)
+    time.sleep(1)
+print(end)
+print("End script3.py:", time.time())
