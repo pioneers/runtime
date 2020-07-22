6125342320c53912fa4e892820370b8992cc8993
Ashwin Vangipuram <ashwinvangipuram@gmail.com>
2020-07-21 21:35:41 -0400

    [BUILD] Fixed CI to rebuild each run - Changed the .travis.yml to copy and build the new files before running - Changed several READMEs - Made executor.c always remove the previous socket - Made tcp_client.c have correct exit code for Docker
    Former-commit-id: 1f2f7712fa0f727ff5556d68e8242a1156f7d9bf

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #/usr/bin/env bash
 
 cd shm_wrapper && ./shm &
-sleep 1
+sleep 0.5
 cd shm_wrapper && ./static_dev &
-sleep 1
+sleep 0.5
 cd net_handler && ./net_handler &
-sleep 1
+sleep 0.5
 cd executor && ./executor &
 wait
