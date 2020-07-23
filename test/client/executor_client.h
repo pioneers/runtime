287d54ca0c16487ea5dbeb2b55a9c0ffeef7a9ef
Ben Liao <benliao1@berkeley.edu>
2020-07-23 02:13:54 -0700

    [TEST] Working shm CLI, added message suppression to net CLI
    - send start pos and run mode working in net CLI
    - shm CLI works, does what shm_print did before
    - executor CLI written but untested (need to test on raspi)
    - made Makefile

---
+++ test/client/executor_client.h
@@ -0,0 +1,10 @@
+#ifndef EXEC_CLIENT_H
+#define EXEC_CLIENT_H
+
+#include "../../runtime_util/runtime_util.h"
+
+void start_executor (char *student_code);
+
+void stop_executor ();
+
+#endif
--- test/client/net_handler_client.h
+++ test/client/net_handler_client.h
@@ -3,9 +3,17 @@
 
 #include "../../runtime_util/runtime_util.h"
 #include "../../net_handler/net_util.h"
+#include <sys/time.h>
 
 #define SHEPHERD_CLIENT 0
 #define DAWN_CLIENT 1
+
+#define IDLE_MODE 0
+#define AUTO_MODE 1
+#define TELEOP_MODE 2
+
+#define LEFT_POS 0
+#define RIGHT_POS 1
 
 typedef struct dev_data {
 	uint64_t uid;    //what the uid of this device is
