9363793c66475e181d1815b2474464b7cc70e06f
Ben Liao <benliao1@berkeley.edu>
2020-07-24 01:29:01 -0700

    [TEST] Filled in almost all of net_handler_client / cli
    - Working send gamepad state from CLI
    - Wrote logic for sending device data from client (untested)
    - Left CLI prompt for device data as TODO
    - Added "view device data" command to print next device data on screen
    - Added documentation to net_handler_client.h

--- test/cli/net_handler_cli.c
+++ test/cli/net_handler_cli.c
@@ -4,7 +4,7 @@
 
 // ********************************** COMMAND-SPECIFIC FUNCTIONS  ****************************** //
 
-void prompt_run_mode()
+void prompt_run_mode ()
 {
 	int client = SHEPHERD_CLIENT;
 	int mode = IDLE_MODE;
@@ -52,7 +52,7 @@ void prompt_run_mode()
 	send_run_mode(client, mode);
 }
 
-void prompt_start_pos()
+void prompt_start_pos ()
 {
 	int client = SHEPHERD_CLIENT;
 	int pos = LEFT_POS;
@@ -85,6 +85,8 @@ void prompt_start_pos()
 		} else if (strcmp(nextcmd, "right\n") == 0) {
 			pos = RIGHT_POS;
 			break;
+		} else if (strcmp(nextcmd, "abort\n") == 0) {
+			return;
 		} else {
 			printf("Invalid response to prompt: %s", nextcmd);
 		}
@@ -95,14 +97,154 @@ void prompt_start_pos()
 	send_start_pos(client, pos);
 }
 
-void prompt_challenge_data()
+void prompt_gamepad_state ()
 {
+	uint32_t buttons = 0;
+	float joystick_vals[4];
+	char nextcmd[MAX_CMD_LEN];
+	char **list_of_names;
+	char button_name[16];
+	int button_ix = 0;
+	int set_joysticks = 0;
 	
+	//get which buttons are pressed
+	printf("Specify which of the following buttons are pressed:\n");
+	list_of_names = get_button_names();
+	for (int i = 0; i < NUM_GAMEPAD_BUTTONS; i++) {
+		printf("\t%s\n", list_of_names[i]);
+	}
+	printf("After you have finished setting buttons, type \"done\"\n");
+	printf("If you accidentally set a button, you can unset the button by typing its name again\n");
+	
+	while (1) {
+		printf("Set/unset this button: ");
+		fgets(nextcmd, MAX_CMD_LEN, stdin);
+		
+		//check for these first
+		if (strcmp(nextcmd, "done\n") == 0) {
+			break;
+		} else if (strcmp(nextcmd, "abort\n") == 0) {
+			return;
+		}
+		
+		//get the index of the button that was specified
+		for (button_ix = 0; button_ix < NUM_GAMEPAD_BUTTONS; button_ix++) {
+			//this logic appends a newline to the end of the button name
+			strcpy(button_name, list_of_names[button_ix]);
+			if (strcmp(nextcmd, strcat(button_name, "\n")) == 0) {
+				break;
+			}
+		}
+		if (button_ix == NUM_GAMEPAD_BUTTONS) {
+			printf("Not a button: %s\n", nextcmd);
+			continue;
+		}
+		
+		//otherwise set that bit in buttons
+		if (buttons & (1 << button_ix)) {
+			buttons &= ~(1 << button_ix);
+			printf("\tOK, unset button %s\n", list_of_names[button_ix]);
+		} else {
+			buttons |= (1 << button_ix);
+			printf("\tOK, set button %s\n", list_of_names[button_ix]);
+		}
+	}
+	
+	//get the joystick values to send
+	printf("Specify the values of the four joysticks (values -1.0 <= val <= 1.0):\n");
+	list_of_names = get_joystick_names();
+	for (int i = 0; i < 4; i++) {
+		printf("\t%s\n", list_of_names[i]);
+	}
+	
+	while (set_joysticks != 4) {
+		printf("Set the value for %s: ", list_of_names[set_joysticks]);
+		fgets(nextcmd, MAX_CMD_LEN, stdin);
+		
+		if (strcmp(nextcmd, "abort\n") == 0) {
+			return;
+		}
+		
+		errno = 0;
+		if (((joystick_vals[set_joysticks] = strtof(nextcmd, NULL)) == 0) && errno != 0) {
+			printf("Input is not a float: %s\n", nextcmd);
+		}
+		if (joystick_vals[set_joysticks] >= 1.0 || joystick_vals[set_joysticks] <= -1.0) {
+			printf("Input is outside of range of joystick: %s\n", nextcmd);
+			continue;
+		}
+		printf("\tOk! %s set to %f\n", list_of_names[set_joysticks], joystick_vals[set_joysticks]);
+		set_joysticks++;
+	}
+	
+	//send
+	printf("Sending Gamepad State message!\n\n");
+	send_gamepad_state(buttons, joystick_vals);
 }
 
-void prompt_device_data()
+void prompt_challenge_data ()
 {
+	int client = SHEPHERD_CLIENT;
+	char nextcmd[MAX_CMD_LEN];
+	char **inputs = malloc(sizeof(char *) * NUM_CHALLENGES);
 	
+	//get client to send as
+	while (1) {
+		printf("Send as DAWN or SHEPHERD: ");
+		fgets(nextcmd, MAX_CMD_LEN, stdin);
+		if (strcmp(nextcmd, "dawn\n") == 0) {
+			client = DAWN_CLIENT;
+			break;
+		} else if (strcmp(nextcmd, "shepherd\n") == 0) {
+			client = SHEPHERD_CLIENT;
+			break;
+		} else if (strcmp(nextcmd, "abort\n") == 0) {
+			return;
+		} else {
+			printf("Invalid response to prompt: %s", nextcmd);
+		}
+	}
+	
+	//get challenge inputs to send
+	for (int i = 1; i <= NUM_CHALLENGES; i++) {
+		//TODO: if we ever put the current names of challenges in runtime_util, make this print better
+		printf("Provide input for challenge %d of %d: ", i, NUM_CHALLENGES); 
+		fgets(nextcmd, MAX_CMD_LEN, stdin);
+		
+		if (strcmp(nextcmd, "abort\n") == 0) {
+			return;
+		}
+		
+		//we need to do this because nextcmd has a newline at the end
+		inputs[i - 1] = malloc(strlen(nextcmd) - 1);
+		for (int j = 0; j < strlen(nextcmd) - 1; j++) {
+			inputs[i - 1][j] = nextcmd[j];
+		}
+	}
+	
+	//send
+	printf("Sending Challenge Data message!\n\n");
+	send_challenge_data(client, inputs);
+	
+	//free pointers
+	for (int i = 0; i < NUM_CHALLENGES; i++) {
+		free(inputs[i]);
+	}
+	free(inputs);
+}
+
+//TODO: fill in this prompt
+void prompt_device_data ()
+{
+	//char nextcmd[MAX_CMD_LEN];
+	
+	//ask for UID
+	//ask for device type
+	//fetch parameters and prompt user "y" or "n" on each one
+	//repeat
+	
+	//send
+	//free pointers
 }
 
 void display_help()
@@ -113,8 +255,10 @@ void display_help()
 	printf("All commands should be typed in all lower case (including when being prompted to send messages)\n");
 	printf("\trun mode           send a Run Mode message\n");
 	printf("\tstart pos          send a Start Pos message\n");
+	printf("\tgamepad state      send a Gamepad State message\n");
 	printf("\tchallenge data     send a Challenge Data message\n");
 	printf("\tdevice data        send a Device Data message (send a subscription request)\n");
+	printf("\tview device data   view the next UDP packet sent to Dawn containing most recent device data\n");
 	printf("\thelp               display this help text\n");
 	printf("\tstop               exit the Net Handler CLI\n");
 }
@@ -154,10 +298,14 @@ int main ()
 			prompt_run_mode();
 		} else if (strcmp(nextcmd, "start pos\n") == 0) {
 			prompt_start_pos();
+		} else if (strcmp(nextcmd, "gamepad state\n") == 0) {
+			prompt_gamepad_state();
 		} else if (strcmp(nextcmd, "challenge data\n") == 0) {
 			prompt_challenge_data();
 		} else if (strcmp(nextcmd, "device data\n") == 0) {
 			prompt_device_data();
+		} else if (strcmp(nextcmd, "view device data\n") == 0) {
+			print_next_dev_data();
 		} else if (strcmp(nextcmd, "help\n") == 0) {
 			display_help();
 		} else {
--- test/client/net_handler_client.h
+++ test/client/net_handler_client.h
@@ -18,19 +18,72 @@
 
 typedef struct dev_data {
 	uint64_t uid;    //what the uid of this device is
+	char *name;      //name of this devices ("KoalaBear", "LimitSwitch", etc.)
 	uint32_t params; //which params to subscribe to
 } dev_data_t;
 
+/*
+ * Starts the real net handler process and connects to all of its outputs
+ * Sets everything up for querying from the CLI or from a test.
+ */
 void start_net_handler ();
 
+/*
+ * Sends SIGINT to the net handler process
+ * Cleans up threads and closes open file descriptors
+ */
 void stop_net_handler ();
 
+/*
+ * Sends a Run Mode message from the specified client with the specified mode
+ * Arguments:
+ *    - int client: one of SHEPHERD_CLIENT or DAWN_CLIENT
+ *    - int mode: one of IDLE_MODE, AUTO_MODE, or TELEOP_MODE
+ * No return value.
+ */
 void send_run_mode (int client, int mode);
 
+/*
+ * Sends a Start Pos message from the specified client with the specified position
+ * Arguments:
+ *    - int client: one of SHEPHERD_CLIENT or DAWN_CLIENT
+ *    - int pos: one of LEFT_POS or RIGHT_POS
+ * No return value.
+ */
 void send_start_pos (int client, int pos);
 
+/*
+ * Sends a Gamepad State message from Dawn over UDP with the specified buttons pushed and joystick values
+ * Arguments:
+ *    - uintt32_t buttons: bitmap of which buttons are pressed. mappings are in runtime_util.h
+ *    - float joystick_vals[4]: values for the four joystick values. mappings are in runtime_util.h
+ * No return value.
+ */
+void send_gamepad_state (uint32_t buttons, float joystick_vals[4]);
+
+/*
+ * Sends a Challenge Data message from the specified client with the specified data
+ * Arguments:
+ *    - int client: one of SHEPHERD_CLIENT or DAWN_CLIENT
+ *    - char **data: array of NUM_CHALLENGES strings, each containing the input to the corresponding challenge
+ * No return value.
+ */
 void send_challenge_data (int client, char **data);
 
-void send_device_data (dev_data_t *data);
+/*
+ * Sends a Device Data message from Dawn over TCP with the specified device subscriptions
+ * Arguments:
+ *    - dev_data_t *data: contains the subscriptions for the devices' parameters
+ *    - int num_devices: contains number of devices for which we are sending subscription requests
+ * No return value.
+ */
+void send_device_data (dev_data_t *data, int num_devices);
+
+/*
+ * Calling this function will let the next device data packet coming into Dawn from Runtime
+ * to be printed to standard output (since device data packets are constantly coming in, they
+ * are normally suppressed; this function unsuppresses one single packet).
+ */
+void print_next_dev_data ();
 
 #endif
