ef42ee4046cd777e64e68a1b808de630e7dac472
Ben Liao <benliao1@berkeley.edu>
2020-07-23 21:11:17 -0700

    [TEST] Failing test with executor CLI added
    - Don't know how to fix issue of executor not knowing where certain modules and files are

---
+++ test/student_code/constant_print.py
@@ -0,0 +1,14 @@
+import time
+
+def constant_print(msg):
+	while True:
+		print(f"{msg} printing again")
+		time.sleep(2)
+
+def autonomous_setup():
+	print('Autonomous setup has begun!')
+	print(f"Starting position: {Robot.start_pos}")
+	Robot.run(constant_print, "autonomous")
+
+def autonomous_main():
+	pass
