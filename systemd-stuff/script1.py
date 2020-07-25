56dbbc8a636e0cac32b4f4a70192e7c0cab6ff33
James Shi <james.shi@berkeley.edu>
2020-07-25 13:38:35 -0700

    [SYSTEMD] Updated service and script files to simulate the current structure of processes

--- systemd-stuff/script1.py
+++ systemd-stuff/script1.py
@@ -2,11 +2,12 @@
 
 import time
 
-end = 10
+cur = time.time()
 
-print("Start script1.py:", time.time())
-for i in range(end):
-    print(i)
+duration = 10
+
+print("Start script1.py:", cur, flush=True)
+for i in range(duration):
+    print(i, flush=True)
     time.sleep(1)
-print(end)
-print("End script1.py:", time.time())
+print(duration, flush=True)
