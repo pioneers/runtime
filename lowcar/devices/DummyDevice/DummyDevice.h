35b08fa49a61b838d2c4b3f7a8e3f2c388043bbe
Ben Liao <benliao1@berkeley.edu>
2020-07-31 19:21:09 -0700

    [LOWCAR] Working PolarBear, started styling lowcar code (incomplete)

--- lowcar/devices/DummyDevice/DummyDevice.h
+++ lowcar/devices/DummyDevice/DummyDevice.h
@@ -4,14 +4,14 @@
 #include "Device.h"
 #include "defs.h"
 
-//DOES NOT REPRESENT ANY ACTUAL DEVICE
-//Use to exercise the full extent of possible params and functionality of a lowcar device from runtime
-//Flashable onto bare Arduino micro for testing
+// DOES NOT REPRESENT ANY ACTUAL DEVICE
+// Use to exercise the full extent of possible params and functionality of a lowcar device from runtime
+// Flashable onto bare Arduino micro for testing
 
 class DummyDevice : public Device
 {
 public:
-	//construct a Dummy device
+	// construct a Dummy device
 	DummyDevice();
 
 	virtual size_t device_read (uint8_t param, uint8_t *data_buf);
@@ -21,27 +21,27 @@ public:
 	virtual void device_actions();
 
 private:
-	int16_t runtime; //param 0
+	int16_t runtime; // param 0
 	float shepherd;
 	uint8_t dawn;
 
-	int16_t devops; //param 3
+	int16_t devops; // param 3
 	float atlas;
 	uint8_t infra;
 
-	int16_t sens; //param 6
+	int16_t sens; // param 6
 	float pdb;
 	uint8_t mech;
 
-	int16_t cpr; //param 9
+	int16_t cpr; // param 9
 	float edu;
 	uint8_t exec;
 
-	int16_t pief; //param 12
+	int16_t pief; // param 12
 	float funtime;
 	uint8_t sheep;
 
-	int16_t dusk; //param 15
+	int16_t dusk; // param 15
 };
 
 #endif
