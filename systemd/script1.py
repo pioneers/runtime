392923886617df7a4700d9e0a0e067612ba3dcf9
James Shi <james.shi@berkeley.edu>
2020-08-01 15:32:03 -0700

    [SYSTEMD] Changed the systemd service files to loop back from test1 to test5 on error

--- systemd/script1.py
+++ systemd/script1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import time
+import sys
 
 cur = time.time()
 
@@ -8,6 +9,8 @@ duration = 10
 
 print("Start script1.py:", cur, flush=True)
 for i in range(duration):
+    if i == 7:
+        sys.exit(1)
     print(i, flush=True)
     time.sleep(1)
 print(duration, flush=True)
