4934a52d176f10572c136a53ecb4291956464a15
Vincent Le <levincent06@berkeley.edu>
2020-08-01 21:42:08 -0700

    [TEST] CLI can connect/disconnect GeneralTestDevices
    - Strip newline characters from stdin
    - Keep track of child processes to kill in client
    - Comment out prints in GeneralTestDevice
    - Bug: list_devices() doesn't show proper device name
    - Bug: Socket inconsistently fails

--- tests/client/dev_handler_client.c
+++ tests/client/dev_handler_client.c
@@ -9,8 +9,9 @@
 // The maximum number of devices that can be connected
 #define MAX_DEVICES 32
 
-// Struct grouping a virtual device's socket number, socket fd, and name
+// Struct grouping a virtual device's process id, socket fd, and name
 typedef struct {
+    pid_t pid;          // The process id of the virtual device
     int fd;             // The socket's file descriptor
     char *dev_exe_name; // The name of the virtual device's executable file
 } device_socket_t;
@@ -24,6 +25,7 @@ device_socket_t used_sockets[MAX_DEVICES];
 __attribute__((constructor))
 void used_sockets_init() {
     for (int i = 0; i < MAX_DEVICES; i++) {
+        used_sockets[i].pid = -1;
         used_sockets[i].fd = -1;
         used_sockets[i].dev_exe_name = "";
     }
@@ -32,20 +34,25 @@ void used_sockets_init() {
 // ******************************** Private ********************************* //
 
 /**
- * Returns a file descriptor after connecting to a socket
+ * Returns the socket number after connecting to an available socket
  * Returns:
- *    A valid file descriptor, or
+ *    The socket number, or
  *    -1 if error
  */
-static int connect_socket(char *dev_exe_name) {
+static int connect_socket() {
     // Get an unoccupied socket and get a fd
-    int socket_num;
+    int socket_num = -1;
     for (int i = 0; i < 32; i++) {
         if (used_sockets[i].fd == -1) {
             socket_num = i;
             break;
         }
     }
+    // Error if no sockets available
+    if (socket_num == -1) {
+        return -1;
+    }
+
     // Make UNIX socket with raw byte streams
     int server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (server_fd < 0) {
@@ -81,7 +88,6 @@ static int connect_socket(char *dev_exe_name) {
 
     // Indicate in global variable that socket is now used
     used_sockets[socket_num].fd = connection_fd;
-    used_sockets[socket_num].dev_exe_name = dev_exe_name;
 
     // Set read() to timeout for up to TIMEOUT milliseconds
     struct timeval tv;
@@ -89,7 +95,7 @@ static int connect_socket(char *dev_exe_name) {
     tv.tv_usec = 0;
     setsockopt(connection_fd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof(tv));
 
-    return connection_fd;
+    return socket_num;
 }
 
 // ******************************** Public ********************************* //
@@ -103,40 +109,58 @@ void stop_dev_handler() {
 }
 
 void connect_device(char* dev_exe_name) {
-    int fd = connect_socket(dev_exe_name);
-    // Spawn virtual device with arg fd
+    int socket_num = connect_socket();
+    used_sockets[socket_num].dev_exe_name = dev_exe_name;
     pid_t pid = fork();
-    if (pid == 0) { // If child process
-        // Become the virtual device
+    if (pid < 0) {
+        printf("connect_device: Couldn't spawn child process %s\n", dev_exe_name);
+    } else if (pid == 0) { // If child process
+        // Cd into virtual_devices dir where the device exe is
+        if (chdir("client/virtual_devices") == -1) {
+            printf("chdir: %s\n", strerror(errno));
+        }
+
+        // Become the virtual device by calling "./<dev_exe_name> <fd>"
         char exe_name[32];
         sprintf(exe_name, "./%s", dev_exe_name);
-        char fd_str[2];
-        sprintf(fd_str, "%d", fd);
-        if (execlp(exe_name, fd_str, (char *) NULL) < 0) {
-            printf("connect_device: execlp failed -- %s\n", strerror(errno));
+        char fd_str[2]; // CLI arg to virtual device
+        sprintf(fd_str, "%d", used_sockets[socket_num].fd);
+
+        if (execlp(exe_name, dev_exe_name, fd_str, (char *) NULL) < 0) {
+            printf("connect_device: execlp %s failed -- %s\n", exe_name, strerror(errno));
         }
+    } else { // Parent
+        used_sockets[socket_num].pid = pid;
     }
 }
 
 void disconnect_device(int socket_num) {
+    // Do nothing if socket is unused
+    if (used_sockets[socket_num].fd == -1) {
+        return;
+    }
+    // Kill virtual device process
+    kill(used_sockets[socket_num].pid, SIGINT);
     // CLose file descriptor
     close(used_sockets[socket_num].fd);
-    // Remove socket
+    // Remove socket (dev handler should recognize disconnect after removal)
     char socket_name[14];
     sprintf(socket_name, "%s%d", SOCKET_PREFIX, socket_num);
     remove(socket_name);
     // Mark socket as unoccupied
+    used_sockets[socket_num].pid = -1;
     used_sockets[socket_num].fd = -1;
     used_sockets[socket_num].dev_exe_name = "";
 }
 
 void list_devices() {
-    printf("Socket\t\t\tDevice\n");
+    printf("SOCKET\tDEVICE\n");
     for (int i = 0; i < MAX_DEVICES; i++) {
         // If socket is used, show information
-        if (used_sockets[i].fd != -1) {
-            printf("%s%d\t\t%s\n", SOCKET_PREFIX, i, used_sockets[i].dev_exe_name);
-        }
+        //if (used_sockets[i].fd != -1) {
+        printf("  %d\t%s\n", i, used_sockets[i].dev_exe_name);
+        fflush(stdout);
+        //}
     }
     return;
 }
