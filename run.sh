7e831968d665766bf4982cdc3e6fb7c6b8cc0d63
Ashwin Vangipuram <ashwinvangipuram@gmail.com>
2020-08-02 22:10:00 -0400

    [BUILD] Fixed run.sh to work with new shm_stop

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+function cleanup() {
+    sleep 0.5
+    cd shm_wrapper && ./shm_stop
+    exit 0
+}
+
 cd shm_wrapper && ./shm_start &
+trap cleanup INT
 sleep 0.5
 cd shm_wrapper && ./static_dev &
 sleep 0.5
@@ -10,4 +17,3 @@ cd executor && ./executor &
 sleep 0.5
 cd dev_handler && ./dev_handler &
 wait
-cd shm_wrapper && ./shm_stop
