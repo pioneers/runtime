7aaec4f5a30aa29f7e0cf55308028ca9001b6f9e
Vincent Le <levincent06@berkeley.edu>
2020-08-05 18:51:26 -0700

    [TEST] Add tc_68_5 and tc_68_6
    - tc_68_5: Simple connect/disconnect
    - tc_68_6: Connect MAX_DEVICES (FAILING)
    
    
    Former-commit-id: e6afc6cd8618058830d36be67a53a0b410241bec

---
+++ tests/integration/tc_68_5.c
@@ -0,0 +1,36 @@
+/**
+ * Simple Hotplugging test to verify a single device connect/disconnect
+ * registers in shared memory
+ */
+#include "../test.h"
+
+char no_device[] = "no connected devices";
+
+int main() {
+    // Setup
+    start_test("Simple Hotplug");
+    start_shm();
+    start_dev_handler();
+
+    // Connect a device then disconnect it
+    print_dev_ids();
+    connect_device("SimpleTestDevice", 0x123);
+    sleep(1);
+    print_dev_ids();
+    disconnect_device(0);   // Device will be at the 0th socket
+    sleep(1);
+    print_dev_ids();
+
+    // Stop all processes
+    stop_dev_handler();
+    stop_shm();
+    end_test();
+
+    // Check outputs
+    in_rest_of_output(no_device);  // Before connect
+    char expected_output[64];
+    sprintf(expected_output, "dev_ix = 0: type = %d", device_name_to_type("SimpleTestDevice"));
+    in_rest_of_output(expected_output);
+    in_rest_of_output(no_device);  // After disconnect
+    return 0;
+}
