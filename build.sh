ab9be1591dcd4dcb5910f1c8ac35063074e83e1c
Ashwin Vangipuram <ashwinvangipuram@gmail.com>
2020-08-12 19:45:29 -0400

    [BUILD] Fixed Makefiles to not rebuild unnecessarily

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -e  # Makes any errors exit the script
 
-pushd executor && make && popd
-pushd net_handler && make && popd
 pushd shm_wrapper && make && popd
+pushd net_handler && make && popd
+pushd executor && make && popd
 pushd dev_handler && make && popd
