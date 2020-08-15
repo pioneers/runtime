0109a50639fa451f4176430f19a004dcc26f8cdd
Vincent Le <levincent06@berkeley.edu>
2020-08-14 22:57:16 -0700

    [TEST] Clarify tc_68_8. Add more checks in tc_68_14
    - Add net_handler to tc_68_8
    - Add clarifying comments to tc_68_14
    - Add more rigorous output checks to tc_68_14

--- tests/integration/tc_68_14.c
+++ tests/integration/tc_68_14.c
@@ -1,68 +1,100 @@
 /**
  * Hotplugging/Performance
  * Connecting different types of devices to "stress test" dev_handler
- * and see if it properly handles the multiple bad devies
- * (Ideally we would connect more devices but the "phenomenom" has said no)
+ * and see if it properly handles the multiple bad devices without affecting
+ * the proper lowcar devices
  */
 #include "../test.h"
 
 char no_device[] = "no connected devices";
 char unknown_device[] = "A non-PiE device was recently plugged in. Please unplug immediately";
-char unknown_message[] = "Couldn't parse message from /tmp/ttyACM0";
+char bad_message[] = "Couldn't parse message from /tmp/ttyACM0";
+
+#define NUM_GENERAL 3
+#define NUM_UNSTABLE 3
 
 int main(){
     // Setup
-    start_test("Multiple Unstable Hotplug");
+    start_test("Hotplug Variety");
     start_shm();
     start_net_handler();
     start_dev_handler();
+    uint8_t general_dev_type = device_name_to_type("GeneralTestDevice");
+    uint8_t unstable_dev_type = device_name_to_type("UnstableTestDevice");
     uint64_t uid = 0;
 
     // Connect general devices
-    print_dev_ids();
-    for(int i = 0; i < 3; i++){
-        uid++;
+    print_dev_ids(); // No devices
+    for (int i = 0; i < NUM_GENERAL; i++) {
         connect_virtual_device("GeneralTestDevice", uid);
+        uid++;
     }
     sleep(1);
-    print_dev_ids();
-    
-    //connect unstable devices then sleep for 8 seconds
-    for(int i = 0; i < 3; i++){
-        uid++;
+    print_dev_ids(); // Only GeneralTestDevices
+
+    // connect unstable devices then sleep for 8 seconds
+    for (int i = 0; i < NUM_UNSTABLE; i++) {
         connect_virtual_device("UnstableTestDevice", uid);
+        uid++;
     }
-    print_dev_ids();
-    sleep(8);
-    print_dev_ids();
+    sleep(1);           // Make sure all devices are registered
+    print_dev_ids();    // NUM_GENERAL + NUM_UNSTABLE devices
+    sleep(5);           // All UnstableTestDevices should time out
+    print_dev_ids();    // Only GeneralTestDevice
     connect_virtual_device("ForeignTestDevice", uid);
     uid++;
     connect_virtual_device("UnresponsiveTestDevice", uid);
+    uid++;
 
-    sleep(8);
-    print_dev_ids();
+    sleep(4);           // ForeignTestDevice and UnresponsiveTestDevice should be denied
+    print_dev_ids();    // Only GeneralTestDevice
 
-
+    // Clean up
     disconnect_all_devices();
-    sleep(1);
-    print_dev_ids();
+    sleep(2);
+    print_dev_ids();    // All devices should be properly disconnected
     sleep(1);
     stop_dev_handler();
     stop_net_handler();
     stop_shm();
     end_test();
 
+    // Check outputs
     in_rest_of_output(no_device);  // No device connected
     char expected_output[64];
-    in_rest_of_output("UnstableTestDevice (0x0000000000000004) timed out!");
-    in_rest_of_output("UnstableTestDevice (0x0000000000000005) timed out!");
-    in_rest_of_output("UnstableTestDevice (0x0000000000000006) timed out!");
-    in_rest_of_output(unknown_device);
-    in_rest_of_output(unknown_device);
-    for (int i = 0; i < 3; i++) {
-        sprintf(expected_output, "dev_ix = %d", i); // check that the only device connetcted are generalTestDevice
+    // Only NUM_GENERAL GeneralTestDevices
+    for (int i = 0; i < NUM_GENERAL; i++) {
+        sprintf(expected_output, "dev_ix = %d: type = %d, year = %d, uid = %llu\n", i, general_dev_type, general_dev_type, i);
         in_rest_of_output(expected_output);
     }
+    // After connecting NUM_UNSTABLE UnstableTestDevices
+    for (int i = 0; i < NUM_GENERAL; i++) {
+        sprintf(expected_output, "dev_ix = %d: type = %d, year = %d, uid = %llu\n", i, general_dev_type, general_dev_type, i);
+        in_rest_of_output(expected_output);
+    }
+    for (int i = 0; i < NUM_UNSTABLE; i++) {
+        sprintf(expected_output, "dev_ix = %d: type = %d, year = %d, uid = %llu\n", i + NUM_GENERAL, unstable_dev_type, unstable_dev_type, i + NUM_GENERAL);
+        in_rest_of_output(expected_output);
+    }
+    // All UnstableTestDevices time out
+    for (int i = 0; i < NUM_UNSTABLE; i++) {
+        sprintf(expected_output, "UnstableTestDevice (0x%016llX) timed out!", i + NUM_GENERAL);
+        in_rest_of_output(expected_output);
+    }
+    // Only GeneralTestDevices remain
+    for (int i = 0; i < NUM_GENERAL; i++) {
+        sprintf(expected_output, "dev_ix = %d: type = %d, year = %d, uid = %llu\n", i, general_dev_type, general_dev_type, i);
+        in_rest_of_output(expected_output);
+    }
+    // ForeignTestDevice and UnresponsiveTestDevice should be denied
+    in_rest_of_output(unknown_device);
+    in_rest_of_output(unknown_device);
+    // Only GeneralTestDevice remain
+    for (int i = 0; i < NUM_GENERAL; i++) {
+        sprintf(expected_output, "dev_ix = %d: type = %d, year = %d, uid = %llu\n", i, general_dev_type, general_dev_type, i);
+        in_rest_of_output(expected_output);
+    }
+    // Disconnecting all device should show no devices
     in_rest_of_output(no_device);
     return 0;
 }
--- tests/integration/tc_68_8.c
+++ tests/integration/tc_68_8.c
@@ -1,36 +1,39 @@
 /**
  * Hotplugging
- * Test to make sure that an 'Foreign' device gets sdoes not get
- * connected to shm despite sending random bytes of data
+ * Verifies that a ForeignTestDevice does not get connected to shared memory.
+ * A ForeignTestDevice does not send an ACK, but sends only random bytes
  */
 #include "../test.h"
 
 char no_device[] = "no connected devices";
 char unknown_device[] = "A non-PiE device was recently plugged in. Please unplug immediately";
-char unknown_message[] = "Couldn't parse message from /tmp/ttyACM0";
+char bad_message[] = "Couldn't parse message from /tmp/ttyACM0";
 
-int main(){
+int main() {
     // Setup
-    start_test("Foreign Hotplug");
+    start_test("Hotplug ForeignTestDevice");
     start_shm();
+    start_net_handler();
     start_dev_handler();
+    sleep(1);
 
-    // Connect a device then disconnect it
-    print_dev_ids();
-    connect_virtual_device("ForeignTestDevice", 0x123);
+    // Connect a ForeignTestDevice
+    print_dev_ids(); // No device
+    connect_virtual_device("ForeignTestDevice", 0x123); // Bad message, then unknown device
     sleep(2);
-    print_dev_ids();
+    print_dev_ids(); // No device
 
+    // Clean up
     disconnect_all_devices();
     stop_dev_handler();
+    stop_net_handler();
     stop_shm();
     end_test();
 
+    // Check outputs
     in_rest_of_output(no_device);
-    in_rest_of_output(unknown_message);
+    in_rest_of_output(bad_message);
     in_rest_of_output(unknown_device);
-    char unexpected_output[64];
-    sprintf(unexpected_output, "dev_ix = 0: type = %d", device_name_to_type("ForeignTestDevice"));
-    not_in_output(unexpected_output);  // No device connected to shm
+    in_rest_of_output(no_device);
     return 0;
 }
