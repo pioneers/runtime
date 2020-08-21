84bd589c29abaf4a248f2d0ee4d20d9262992842
Vincent Le <levincent06@berkeley.edu>
2020-08-21 14:06:16 -0700

    [MISC] Fix style and document Device directory
    Former-commit-id: 0338948f325d5ac5f86ff085d84c60077721ccdd

--- lowcar/devices/Device/Device.cpp
+++ lowcar/devices/Device/Device.cpp
@@ -4,126 +4,119 @@ const float Device::MAX_SUB_INTERVAL_MS = 500.0;    // maximum tolerable subscri
 const float Device::MIN_SUB_INTERVAL_MS = 40.0;     // minimum tolerable subscription delay, in ms
 
 // Device constructor
-Device::Device(DeviceType dev_id, uint8_t dev_year, uint32_t timeout, uint32_t ping_interval) {
+Device::Device(DeviceType dev_id, uint8_t dev_year, uint32_t timeout) {
     this->dev_id.type = dev_id;
     this->dev_id.year = dev_year;
     this->dev_id.uid = 0; // sets a temporary value
 
-	this->params = 0;          // nothing subscribed to right now
-	this->sub_interval = 0;    // 0 acts as flag indicating no subscription
-	this->timeout = timeout;   // timeout in ms how long we tolerate not receiving a message from dev handler
-	this->enabled = FALSE;
+    this->params = 0;          // Initialize to no subscribed parameters (empty DEVICE_DATA)
+    this->sub_interval = 0;    // 0 acts as flag indicating no subscription
+    this->timeout = timeout;   // timeout in ms how long we tolerate not receiving a PING from dev handler
+    this->enabled = FALSE;
 
     this->msngr = new Messenger();
     this->led = new StatusLED();
 
     device_enable(); // call device's enable function
 
-	this->last_sent_data_time = this->last_received_ping_time = this->curr_time = millis();
+    this->last_sent_data_time = this->last_received_ping_time = this->curr_time = millis();
 }
 
-// Sets the UID
 void Device::set_uid (uint64_t uid) {
     this->dev_id.uid = uid;
 }
 
-//universal loop function
 void Device::loop () {
-	Status sts;
-	this->curr_time = millis();
-	sts = this->msngr->read_message(&(this->curr_msg)); //try to read a new message
+    Status sts;
+    this->curr_time = millis();
+    sts = this->msngr->read_message(&(this->curr_msg)); // try to read a new message
 
-	if (sts == Status::SUCCESS) { //we have a message!
-		switch (this->curr_msg.message_id) {
-			case MessageID::PING:
-				this->last_received_ping_time = this->curr_time;
-				// If this is the first PING received, send an ACKNOWLEDGEMENT
-				if (!this->enabled) {
-					this->msngr->lowcar_printf("Device type %d with UID ending in %X contacted; sending ACK", this->dev_id.type, this->dev_id.uid);
-					this->msngr->send_message(MessageID::ACKNOWLEDGEMENT, &(this->curr_msg), &(this->dev_id));
-					this->enabled = TRUE;
-				}
-				break;
+    if (sts == Status::SUCCESS) { // we have a message!
+        switch (this->curr_msg.message_id) {
+            case MessageID::PING:
+                this->last_received_ping_time = this->curr_time;
+                // If this is the first PING received, send an ACKNOWLEDGEMENT
+                if (!this->enabled) {
+                    this->msngr->lowcar_printf("Device type %d with UID ending in %X contacted; sending ACK", this->dev_id.type, this->dev_id.uid);
+                    this->msngr->send_message(MessageID::ACKNOWLEDGEMENT, &(this->curr_msg), &(this->dev_id));
+                    this->enabled = TRUE;
+                }
+                break;
 
-			case MessageID::SUBSCRIPTION_REQUEST:
-				this->params = *((uint32_t *) this->curr_msg.payload);    // Update subscribed params
-				this->sub_interval = *((uint16_t *) (this->curr_msg.payload + PARAM_BITMAP_BYTES));  // Update the interval at which we send DEVICE_DATA
-				// Make sure sub_interval is within bounds
-				this->sub_interval = min(this->sub_interval, MAX_SUB_INTERVAL_MS);
-				this->sub_interval = max(this->sub_interval, MIN_SUB_INTERVAL_MS);
-				break;
+            case MessageID::SUBSCRIPTION_REQUEST:
+                this->params = *((uint32_t *) this->curr_msg.payload);    // Update subscribed params
+                this->sub_interval = *((uint16_t *) (this->curr_msg.payload + PARAM_BITMAP_BYTES));  // Update the interval at which we send DEVICE_DATA
+                // Make sure sub_interval is within bounds
+                this->sub_interval = min(this->sub_interval, MAX_SUB_INTERVAL_MS);
+                this->sub_interval = max(this->sub_interval, MIN_SUB_INTERVAL_MS);
+                break;
 
-			case MessageID::DEVICE_WRITE:
-				device_write_params(&(this->curr_msg));
-				break;
+            case MessageID::DEVICE_WRITE:
+                device_write_params(&(this->curr_msg));
+                break;
 
-			// Receiving some other Message
-			default:
-				this->msngr->lowcar_printf("Unrecognized message received by lowcar device");
-				break;
-		}
-	} else if (sts != Status::NO_DATA) {
-		this->msngr->lowcar_printf("Error when reading message by lowcar device");
-	}
+            // Receiving some other Message
+            default:
+                this->msngr->lowcar_printf("Unrecognized message received by lowcar device");
+                break;
+        }
+    } else if (sts != Status::NO_DATA) {
+        this->msngr->lowcar_printf("Error when reading message by lowcar device");
+    }
 
     // If it's been too long since we received a PING, disable the device
-	if ((this->timeout > 0)  && (this->curr_time - this->last_received_ping_time >= this->timeout)) {
-		device_disable();
-		this->enabled = FALSE;
-	}
+    if ((this->timeout > 0)  && (this->curr_time - this->last_received_ping_time >= this->timeout)) {
+        device_disable();
+        this->enabled = FALSE;
+    }
 
-	// If we still haven't gotten our first PING yet (or dev handler timed out), keep waiting for a PING
-	if (!(this->enabled)) {
-		return;
-	}
+    // If we still haven't gotten our first PING yet (or dev handler timed out), keep waiting for a PING
+    if (!(this->enabled)) {
+        return;
+    }
 
     // do device-specific actions. This may change params
     device_actions();
 
-	/* Send another DEVICE_DATA with subscribed parameters if this->sub_interval
+    /* Send another DEVICE_DATA with subscribed parameters if this->sub_interval
      * milliseconds passed since the last time we sent a DEVICE_DATA
-	 * If this->sub_interval == 0, don't send DEVICE_DATA
+     * If this->sub_interval == 0, don't send DEVICE_DATA
      * Note that it is possible that no parameters are subscribed.
      * We send an "empty" DEVICE_DATA anyways to let dev handler know we're still online
      */
-	if ((this->sub_interval > 0) && (this->curr_time - this->last_sent_data_time >= this->sub_interval)) {
-		this->last_sent_data_time = this->curr_time;
-		device_read_params(&(this->curr_msg));
-		this->msngr->send_message(MessageID::DEVICE_DATA, &(this->curr_msg));
-	}
+    if ((this->sub_interval > 0) && (this->curr_time - this->last_sent_data_time >= this->sub_interval)) {
+        this->last_sent_data_time = this->curr_time;
+        device_read_params(&(this->curr_msg));
+        this->msngr->send_message(MessageID::DEVICE_DATA, &(this->curr_msg));
+    }
 
-	// Send any queued logs
-	this->msngr->lowcar_flush();
+    // Send any queued logs
+    this->msngr->lowcar_flush();
 }
 
-//************************************************ DEFAULT DEVICE-SPECIFIC METHODS ******************************************* //
+// ******************** DEFAULT DEVICE-SPECIFIC METHODS ********************* //
 
-// a device uses this function to return data about its state
 size_t Device::device_read (uint8_t param, uint8_t *data_buf) {
     return 0; // by default, we read 0 bytes into buffer
 }
 
-// a device uses this function to change a state
 size_t Device::device_write (uint8_t param, uint8_t *data_buf) {
     return 0; // by default, we wrote 0 bytes successfully to device
 }
 
-// a device uses this function to enable itself
 void Device::device_enable () {
     return; // by default, enabling the device does nothing
 }
 
-// a device uses this function to disable itself
 void Device::device_disable () {
     return; // by default, disabling the device does nothing
 }
 
-// a device uses this function to perform any continuous updates or actions
 void Device::device_actions () {
     return; // by default, device does nothing on every loop
 }
 
-//*************************************************** HELPER METHODS ***************************************************//
+// ***************************** HELPER METHODS ***************************** //
 
 void Device::device_read_params (message_t *msg) {
     // Clear the message before building device data
--- lowcar/devices/Device/Messenger.h
+++ lowcar/devices/Device/Messenger.h
@@ -1,4 +1,4 @@
-/*
+/**
  * A class used by each device to send and receive messages to and from the device handler
  */
 
@@ -13,30 +13,58 @@ public:
     /* constructor; opens Serial connection */
     Messenger();
 
-    /* Handles any type of message and fills in appropriate parameters, then sends onto Serial port
-     * msg_id: The MessageID to populate msg->message_id
-     * msg: The message to send
-     * dev_id: The id of the device. Used only for ACKNOWLEDGEMENT messages
-     *
-     * Return a Status enum to report on success/failure
-     * Resets msg->message_id and msg->payload_length
+    /**
+     * Handles any type of message and fills in appropriate parameters, then sends onto Serial port
+     * Clears all of MSG's fields before returning.
+     * Arguments:
+     *    msg_id: The MessageID to populate msg->message_id
+     *    msg: The message to send
+     *    dev_id: The id of the device. Used for only ACKNOWLEDGEMENT messages
+     * Returns:
+     *    a Status enum to report on success/failure
      */
     Status send_message(MessageID msg_id, message_t *msg, dev_id_t *dev_id = NULL);
 
-    /* Reads in data from serial port and puts results into msg
-     * Return a Status enum to report on success/failure
+    /**
+     * Reads in data from serial port and puts results into msg
+     * Arguments:
+     *    msg: A message to be populated based on read serial data.
+     * Returns:
+     *    a Status enum to report on success/failure
      */
     Status read_message(message_t *msg);
 
-    // Logging
-    void lowcar_printf(char* format, ...);            // Queues a log. Same usage as printf()
-    void lowcar_print_float(char *name, float val);   // Workaround to print a float value
-    void lowcar_flush();                              // Sends queued logs over serial
+    // ****************************** LOGGING ******************************* //
+
+    /**
+     * Queues a LOG message to be sent to dev handler (for runtime logger)
+     * Arguments:
+     *    format: The string format to be sent (same usage as printf())
+     */
+    void lowcar_printf(char* format, ...);
+
+    /**
+     * Workaround to print a float value.
+     * Use this function to print the value of a float instead of %f in lowcar_printf()
+     * This is needed because floats appear as question marks in the logger
+     * Sends a log with the string: "<name>: <val>"
+     *  Where val is displayed up to the thousandths place
+     * Arguments:
+     *    name: A string describing the value of the float (ex: the variable name)
+     *    val: The float to be printed alongside NAME
+     */
+    void lowcar_print_float(char *name, float val);
+
+    /**
+     * Sends all currently queued logs to dev handler
+     * The log queue is then cleared.
+     */
+    void lowcar_flush();
 
 private:
     // protocol constants
-    const static int DELIMITER_BYTES;
-    const static int COBS_LEN_BYTES;
+    const static int DELIMITER_BYTES;   // The size of a packet delimiter
+    const static int COBS_LEN_BYTES;    // The size the cobs length in a packet
 
     const static int MESSAGEID_BYTES;    // bytes in message ID field of packet
     const static int PAYLOAD_SIZE_BYTES; // bytes in payload size field of packet
@@ -47,15 +75,66 @@ private:
     const static int DEV_ID_UID_BYTES;   // bytes in uid field of dev_id
 
     // private variables
-    uint8_t log_queue_max_size;
-    char **log_queue;
-    uint8_t num_logs;
+    uint8_t log_queue_max_size;         // The size of the log queue in bytes
+    char **log_queue;                   // The log queue
+    uint8_t num_logs;                   // The number of logs in the log queue
 
-    // helper methods; see source file for more detailed description of functionality
+    // *************************** HELPER METHODS *************************** //
+
+    /**
+     * Appends data to the payload of a message
+     * The payload length field of the message is incremented accordingly.
+     * Arguments:
+     *    msg: The message whose payload is to be appended to
+     *    data: The buffer containing data to append to msg->payload
+     *    length: The size of DATA
+     * Returns:
+     *    0 on success, or
+     *    -1 if LENGTH is too large (DATA can't fit in the allocated payload)
+     */
     int append_payload(message_t *msg, uint8_t *data, uint8_t length);
+
+    /**
+     * Serializes a message into a byte packet to be sent over serial.
+     * Arguments:
+     *    data: The buffer to write the serialized message into
+     *    msg: A populated message to be serialized.
+     */
     void message_to_byte(uint8_t *data, message_t *msg);
+
+    /**
+     * Computes the checksum of a data buffer.
+     * The checksum is the bitwise XOR of each byte in the buffer.
+     * Arguments:
+     *    data: The data buffer whose checksum is to be computed.
+     *    length: the size of DATA
+     * Returns:
+     *    the checksum
+     */
     uint8_t checksum(uint8_t *data, int length);
+
+    // **************************** COBS ENCODING *************************** //
+
+    /**
+     * Cobs encodes a byte array into a buffer
+     * Arguments:
+     *    src: The byte array to be encoded
+     *    dst: The buffer to write the encoded data into
+     *    src_len: The size of SRC
+     * Returns:
+     *    The size of the encoded data, DST
+     */
     size_t cobs_encode(uint8_t *dst, const uint8_t *src, size_t src_len);
+
+    /**
+     * Cobs decodes a byte array into a buffer
+     * Arguments:
+     *    src: The byte array to be decoded
+     *    dst: The buffer to write the decoded data into
+     *    src_len: The size of SRC
+     * Returns:
+     *    The size of the decoded data, DST
+     */
     size_t cobs_decode(uint8_t *dst, const uint8_t *src, size_t src_len);
 };
 
