8213292ddcf7bf3d6528daf33e34d87145a37d41
Vincent Le <levincent06@berkeley.edu>
2020-09-14 23:58:58 -0700

    [MISC] Remove sleep in net_handler_client send_gamepad
    - Clean up keyboard_interface

--- tests/cli/keyboard_interface.c
+++ tests/cli/keyboard_interface.c
@@ -8,19 +8,7 @@
 #include <arpa/inet.h>  // for inet_addr, bind, listen, accept, socket types
 #include <netinet/in.h> // for structures relating to IPv4 addresses
 #include <netdb.h>      // for struct addrinfo
-#define SA struct sockaddr
-#define LOCALHOST "192.168.65.2"
 #define PORT 5006 // This port must be exposed in docker-compose.yml
-
-
-char *file = "/cli/gamepad_inputs.fifo";
-int fd;
-FILE *fp;
-char character;
-u_int32_t buttons = 0;
-float joystick_vals[4] = {0};
-void set_state(int set);
-char buff[33];
 
 // ********************************** MAIN PROCESS ****************************************** //
 void sigint_handler(int signum) {
@@ -28,10 +16,6 @@ void sigint_handler(int signum) {
     stop_net_handler();
     exit(0);
 }
-void set_state(int set){
-    buttons |= (1 << set);
-}
-
 
 int connect_tcp(){
     int sockfd, connfd;
@@ -43,38 +27,40 @@ int connect_tcp(){
         printf("socket creation failed...\n");
         exit(0);
     }
-    printf("Socket creation successful\n");
 
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(5006);
+    servaddr.sin_port = htons(PORT);
 
     if((bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr))) != 0){
         printf("socket bind failed... %s\n", strerror(errno));
         exit(0);
     }
-    printf("socket binded successfully...\n");
 
+    printf("Waiting for client\n");
     if((listen(sockfd, 5)) != 0){
         printf("listen failed...\n");
         exit(0);
     }
-    printf("Got client\n");
     socklen_t len = sizeof(cli);
     connfd = accept(sockfd, (struct sockaddr*)&cli, &len);
-    printf("Finished accept function \n");
-    if(connfd < 0){
+    printf("Accepted client\n");
+    if (connfd < 0){
         printf("server accept failed...\n");
         exit(0);
     }
-    printf("server accept the client...\n");
     return connfd;
 }
 
 int main() {
-
     signal(SIGINT, sigint_handler);
-    fd = connect_tcp();
+
+    // The bitmap of buttons pressed to be sent to net handler
+    uint32_t buttons = 0;
+    float joystick_vals[4] = {0};
+    // The received bitstring from Python to be parsed into BUTTONS
+    char buff[33];
+    int fd = connect_tcp();
 
     printf("Its gamer time\n");
 
@@ -86,20 +72,27 @@ int main() {
     // Connect dummy gamepad and start teleop mode
     send_gamepad_state(0, joystick_vals);
     send_run_mode(DAWN, TELEOP);
-    while(1){
+    sleep(1);
+
+    // Receive a bitstring of buttons and parse it
+    while(1) {
+        // Reset buttons/joysticks
         buttons = 0;
         memset(buff, 0, 32 * sizeof(char));
         memset(joystick_vals, 0, 4 * sizeof(float));
+        // Receive bit string
         recv(fd, buff, sizeof(buff), 0);
         buff[32] = '\0';
+        printf("TCP received Buffer: %s\n", buff);
 
+        // Parse joystick values
         for(int i = 13; i < 21; i++){
             int pushed = 0;
             if (buff[i] == '1'){
                 pushed = 1.0;
             }
-            if(pushed != 0){
-                switch(i){
+            if (pushed != 0){
+                switch (i) {
 
                     case 13:
                         joystick_vals[0] = pushed;
@@ -129,13 +122,15 @@ int main() {
             }
         }
 
-        for(int i = 0; i < 13; i++){
-            if(buff[i] == '1'){
+        // Parse the buttons TODO: EXTEND TO BUTTON_XBOX
+        for(int i = BUTTON_A; i < DPAD_DOWN; i++){
+            if (buff[i] == '1'){
                 buttons |= (1 << i);
             }
         }
 
         send_gamepad_state(buttons, joystick_vals);
+        printf("SENT GAMEPAD STATE: %d\n", buttons);
 
     }
     return 0;
