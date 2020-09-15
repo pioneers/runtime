b3f89832e1e3cda910054753d36345d9e6617ca6
Daniel <danielm_124@berkeley.edu>
2020-09-14 18:26:34 -0700

    [MISC]Keyboard interface communicates with tcp -Sends string of bits from python to C -C parses the string and shifts the bits according to char position

--- tests/cli/keyboard_inputs.py
+++ tests/cli/keyboard_inputs.py
@@ -5,120 +5,97 @@ from sys import exit
 from pynput import keyboard
 from pynput.keyboard import Listener, Key
 from time import sleep
-
+import socket       # for TCP
+import struct       # for TCP encoding
 
 file = "gamepad_inputs.fifo"
+# TCP Setup
+HOST = '127.0.0.1'
+PORT = 5006
+ADDRESS = (HOST, PORT)
 
-
+def connect_tcp():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('waiting for server')
+    sock.connect(ADDRESS)
+    print('connected to server')
+    return sock
+    
 def handler(signal_received, frame):
     print("Exiting gracefully")
-    if (fifo.closed == False):
-        fifo.close()
     exit(0)
 
-def setup_fifo():
-    if(os.path.exists(file)):
-        os.remove(file)
-    os.mkfifo(file, 0o660)
-    global fifo
-    fd = os.open(file, os.O_RDWR)
-    fifo = os.fdopen(fd, 'w')
+
+def activate_bit(on):
+    bits[on] = "1"
+
+def deactivate_bit(off):
+    bits[off] = "0"
 
 def set_controls():
-    controls.add(keyboard.KeyCode(char='u'))
-    controls.add(keyboard.KeyCode(char='i'))
-    controls.add(keyboard.KeyCode(char='o'))
-    controls.add(keyboard.KeyCode(char='p'))
+    global controls
+    controls = {
+        keyboard.KeyCode(char="u") : 0,
+        keyboard.KeyCode(char="i") : 1,
+        keyboard.KeyCode(char="o") : 2,
+        keyboard.KeyCode(char="p") : 3,
+        keyboard.KeyCode(char="h") : 4,
+        keyboard.KeyCode(char="j") : 5,
+        keyboard.KeyCode(char="k") : 6,
+        keyboard.KeyCode(char="l") : 7,
+        keyboard.KeyCode(char="f") : 8,
+        keyboard.KeyCode(char="g") : 9,
+        keyboard.KeyCode(char="r") : 10,
+        keyboard.KeyCode(char="t") : 11,
+        keyboard.KeyCode(char="z") : 12,
+        keyboard.KeyCode(char="w") : 13,
+        keyboard.KeyCode(char="s") : 14,
+        keyboard.KeyCode(char="a") : 15,
+        keyboard.KeyCode(char="d") : 16,
 
-    controls.add(keyboard.KeyCode(char='w'))
-    controls.add(keyboard.KeyCode(char='a'))
-    controls.add(keyboard.KeyCode(char='d'))
-    controls.add(keyboard.KeyCode(char='s'))
-
-
-    controls.add(keyboard.KeyCode(char='h'))
-    controls.add(keyboard.KeyCode(char='j'))
-    controls.add(keyboard.KeyCode(char='k'))
-    controls.add(keyboard.KeyCode(char='l'))
-
-    controls.add(keyboard.KeyCode(char='f'))
-    controls.add(keyboard.KeyCode(char='g'))
-    controls.add(keyboard.KeyCode(char='r'))
-    controls.add(keyboard.KeyCode(char='t'))
-
-    controls.add(keyboard.KeyCode(char='z'))
-
-
-    special_keys.add(keyboard.Key.down)
-    special_keys.add(keyboard.Key.up)
-    special_keys.add(keyboard.Key.right)
-    special_keys.add(keyboard.Key.left)
+        keyboard.Key.left : 17,
+        keyboard.Key.right : 18,
+        keyboard.Key.down : 19,
+        keyboard.Key.up : 20
+    }
 
 def on_press(key):
     mutex.acquire()
     if key in controls:
-        keys_held_down.add(key.char)
-    elif key in special_keys:
-        if key == keyboard.Key.up:
-            keys_held_down.add(":V")
-
-        elif key == keyboard.Key.right:
-            keys_held_down.add(":B")
-
-        elif key == keyboard.Key.left:
-            keys_held_down.add(":N")
-
-        elif key == keyboard.Key.down:
-            keys_held_down.add(":M")
+        activate_bit(controls[key])
     mutex.release()
 
 def on_release(key):
     mutex.acquire()
     if key in controls:
-        keys_held_down.remove(key.char)
-
-    elif key == keyboard.Key.up:
-        keys_held_down.remove(":V")
-
-    elif key == keyboard.Key.right:
-        keys_held_down.remove(":B")
-
-    elif key == keyboard.Key.left:
-        keys_held_down.remove(":N")
-
-    elif key == keyboard.Key.down:
-        keys_held_down.remove(":M")
+        deactivate_bit(controls[key])
 
     elif key == keyboard.Key.esc:
-        fifo.close()
+        mutex.release()
         return False
     mutex.release()
 
 def keyboard_control():
-    writer = threading.Thread(target = write_to_fifo)
+    writer = threading.Thread(target = write_to_socket)
     writer.start()
     with Listener(on_press = on_press,on_release = on_release) as listener:
         listener.join()
 
-def write_to_fifo():
+def write_to_socket():
+    sock = connect_tcp()
     while(True):
         mutex.acquire()
-        for key_pressed in keys_held_down:
-            fifo.write(key_pressed)
+        string_to_send = ''.join(bits)
+        print("sending", string_to_send)
+        sock.send(string_to_send.encode())
         mutex.release()
         sleep(0.05)
 
 
 def main():
     signal(SIGINT, handler)
-
-    global controls
-    global special_keys
-    special_keys = set()
-    controls = set()
-    setup_fifo()
-    global keys_held_down
-    keys_held_down = set()
+    global bits
+    bits = list("00000000000000000000000000000000")
     set_controls()
     global mutex
     mutex = threading.Lock()
--- tests/cli/keyboard_interface.c
+++ tests/cli/keyboard_interface.c
@@ -1,119 +1,135 @@
 #include "../client/net_handler_client.h"
 
 #include <stdio.h>
+#include <arpa/inet.h>  // for inet_addr, bind, listen, accept, socket types
+#include <netinet/in.h> // for structures relating to IPv4 addresses
+#include <netdb.h>      // for struct addrinfo
+#define SA struct sockaddr 
+#define LOCALHOST "192.168.65.2"
+#define PORT 5006 // This port must be exposed in docker-compose.yml
 
-char *file = "gamepad_inputs.fifo";
-FILE *fd;
+
+char *file = "/cli/gamepad_inputs.fifo";
+int fd;
+FILE *fp;
 char character;
 u_int32_t buttons = 0;
 float joystick_vals[4];
 void set_state(int set);
+char buff[33];
+
 // ********************************** MAIN PROCESS ****************************************** //
 void sigint_handler(int signum) {
     close_output();
+    stop_net_handler();
     exit(0);
+}
+void set_state(int set){
+    buttons |= (1 << set);
+}
+
+
+int connect_tcp(){
+    int sockfd, connfd;
+    struct sockaddr_in servaddr = {0}; 
+    struct sockaddr_in cli = {0};
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    printf("Socket creation successful\n");
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    servaddr.sin_port = htons(5006);
+
+    if((bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr))) != 0){
+        printf("socket bind failed... %s\n", strerror(errno));
+        exit(0);
+    }
+    printf("socket binded successfully...\n");
+
+    if((listen(sockfd, 5)) != 0){
+        printf("listen failed...\n");
+        exit(0);
+    }
+    printf("Got client\n");
+    socklen_t len = sizeof(cli);
+    connfd = accept(sockfd, (struct sockaddr*)&cli, &len);
+    printf("Finished accept function \n");
+    if(connfd < 0){
+        printf("server accept failed...\n");
+        exit(0);
+    }
+    printf("server accept the client...\n");
+    return connfd;
 }
 
 int main() {
 
     signal(SIGINT, sigint_handler);
+    fd = connect_tcp();
 
-    fd = fopen(file, "r");
-    if(fd == NULL){
-        printf("file not opened \n cleaning up \n");
-        stop_net_handler();
-        close_output();
-        exit(0);
-    }
-
-    printf("Its gamer time...\n");
+    printf("Its gamer time\n");
+    
+    printf("Starting Net Handler CLI...\n");
     fflush(stdout);
 
-    // start the net handler and connect all of its output locations to file descriptors in this process
     start_net_handler();
+
     while(1){
-        printf("Getting character");
-        int set = -1;
-        character = fgetc(fd);
-        if(character == EOF){
-            break;
-        }
-        if(character == 'u'){
-            set = 0;
-        }
-        else if(character == 'i'){
-            set = 1;
-        }
-        else if(character == 'o'){
-            set = 2;
-        }
-        else if(character == 'p'){
-            set = 3;
-        }
-        else if(character == 'h'){
-            set = 4;
-        }
-        else if(character =='j'){
-            set = 5;
-        }
-        else if(character == 'k'){
-            set = 6;
-        }
-        else if(character == 'l'){
-            set = 7;
-        }
-        else if(character == 'f'){
-            set = 8;
-        }
-        else if(character == 'g'){
-            set = 9;
-        }
-        else if(character == 'r'){
-            set = 10;
-        }
-        else if(character == 't'){
-            set = 11;
-        }
-        else if(character == 'w'){
-            set = 12;
-        }
-        else if(character == 's'){
-            set = 13;
-        }
-        else if(character == 'a'){
-            set = 14;
-        }
-        else if(character == 'd'){
-            set = 15;
-        }
-        else if(character == 'z'){
-            set = 16;
-        }
-        else if (character == ':'){
+        buttons = 0;
+        memset(buff, 0, 32 * sizeof(char));
+        memset(joystick_vals, 0, 4 * sizeof(float));
+        recv(fd, buff, sizeof(buff), 0);
+        buff[32] = '\0';
+        
+        for(int i = 13; i < 21; i++){
+            int pushed = 0;
+            if (buff[i] == '1'){
+                pushed = 1.0;
+            }
+            if(pushed != 0){
+                switch(i){
+                    
+                    case 13:
+                        joystick_vals[0] = pushed;
+                        break;
+                    case 14:
+                        joystick_vals[0] = -1 * pushed;
+                        break;
+                    case 15:
+                        joystick_vals[1] = -1 * pushed;
+                        break;
+                    case 16:
+                        joystick_vals[1] = 1 * pushed;
+                        break;
+                    case 17:
+                        joystick_vals[2] = -1 * pushed;
+                        break;
+                    case 18:
+                        joystick_vals[2] = 1 * pushed;
+                        break;
+                    case 19:
+                        joystick_vals[3] = -1 * pushed;
+                        break;
+                    case 20:
+                        joystick_vals[3] = pushed;
+                        break;
+                }
+            }
+        } 
 
-            character = fgetc(fd);
-
-            if(character == 'B'){
-                joystick_vals[0] = 1;
-            }
-            else if(character == 'N'){
-                joystick_vals[0] = -1;
-            }
-            else if(character == 'V'){
-                joystick_vals[1] = 1;
-            }
-            else if(character == 'M'){
-                joystick_vals[1] = -1;
+        for(int i = 0; i < 13; i++){
+            if(buff[i] == '1'){
+                buttons |= (1 << i);
             }
         }
-        if(set != -1){
-            set_state(set);
-        }
-        printf("the button pressed is %d \n", set);
+    
         send_gamepad_state(buttons, joystick_vals);
-    }
+        
+    } 
     return 0;
 }
-void set_state(int set){
-        buttons |= (1 << set);
-    }
