473acf291de031269459b9bb26fc70a934a16b93
Daniel <danielm_124@berkeley.edu>
2020-09-15 18:36:20 -0700

    [MISC] Changed gamepad values
    Former-commit-id: cf714c89091403dded4f429f43f5912f3ce6a362

--- tests/cli/keyboard_inputs.py
+++ tests/cli/keyboard_inputs.py
@@ -35,28 +35,31 @@ def deactivate_bit(off):
 def set_controls():
     global controls
     controls = {
-        keyboard.KeyCode(char="u") : 0,
-        keyboard.KeyCode(char="i") : 1,
-        keyboard.KeyCode(char="o") : 2,
-        keyboard.KeyCode(char="p") : 3,
-        keyboard.KeyCode(char="h") : 4,
-        keyboard.KeyCode(char="j") : 5,
-        keyboard.KeyCode(char="k") : 6,
-        keyboard.KeyCode(char="l") : 7,
-        keyboard.KeyCode(char="f") : 8,
-        keyboard.KeyCode(char="g") : 9,
-        keyboard.KeyCode(char="r") : 10,
-        keyboard.KeyCode(char="t") : 11,
-        keyboard.KeyCode(char="z") : 12,
-        keyboard.KeyCode(char="w") : 13,
-        keyboard.KeyCode(char="s") : 14,
-        keyboard.KeyCode(char="a") : 15,
-        keyboard.KeyCode(char="d") : 16,
-
-        keyboard.Key.left : 17,
-        keyboard.Key.right : 18,
-        keyboard.Key.down : 19,
-        keyboard.Key.up : 20
+        keyboard.KeyCode(char="k") : 0,
+        keyboard.KeyCode(char="l") : 1,
+        keyboard.KeyCode(char="j") : 2,
+        keyboard.KeyCode(char="i") : 3,
+        keyboard.KeyCode(char="u") : 4,
+        keyboard.KeyCode(char="o") : 5,
+        keyboard.KeyCode(char="y") : 6,
+        keyboard.KeyCode(char="p") : 7,
+        keyboard.KeyCode(char="n") : 8,
+        keyboard.KeyCode(char="m") : 9,
+        keyboard.KeyCode(char="q") : 10,
+        keyboard.KeyCode(char="e") : 11,
+        keyboard.KeyCode(char="t") : 12,
+        keyboard.KeyCode(char="g") : 13,
+        keyboard.KeyCode(char="f") : 14,
+        keyboard.KeyCode(char="h") : 15,
+        keyboard.KeyCode(char="z") : 16,
+        keyboard.KeyCode(char="d") : 17,
+        keyboard.KeyCode(char="a") : 18,
+        keyboard.KeyCode(char="s") : 19,
+        keyboard.KeyCode(char="w") : 20,
+        keyboard.Key.left : 21,
+        keyboard.Key.right : 22,
+        keyboard.Key.down : 23,
+        keyboard.Key.up : 24
     }
 
 def on_press(key):
