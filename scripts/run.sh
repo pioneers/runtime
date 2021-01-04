8ea897909fd4a8db686d86f83b096274b9707010
Ben Liao <benliao1@berkeley.edu>
2021-01-04 01:17:38 -0800

    [SYSTEMD] Changes to implement circular restarting of Runtime
    - Changed type of net_handler, dev_handler, and executor services to "simple"
    - Listed shm_start.service as an OnFailure dependency of shm_stop
    - Listed shm_stop.service as conflicting with the three main Runtime processes
    - Was working on a different solution before and added some comments to run.sh
    
    
    Former-commit-id: 05e05d2742b0e8468034c85fab3f657267b9a2a9

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+# This script runs all of Runtime in a terminal
+
+# function to clean up shared memory
 function cleanup() {
     sleep 0.5
     cd shm_wrapper && ./shm_stop
@@ -7,13 +10,15 @@ function cleanup() {
 }
 
 cd shm_wrapper && ./shm_start &
-trap cleanup INT
+trap cleanup INT # sets cleanup as Ctrl-C handler
 sleep 0.5
-#cd shm_wrapper && ./static_dev &
-#sleep 0.5
+
+# starts all three Runtime processes in the background
 cd net_handler && ./net_handler &
 sleep 0.5
 cd executor && ./executor &
 sleep 0.5
 cd dev_handler && ./dev_handler &
+
+# waits for background processes to stop (they won't ever stop)
 wait
