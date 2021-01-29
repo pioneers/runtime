c933aa1d24da15766a3e56746c72a98f090a7eb9
Ben Liao <benliao1@berkeley.edu>
2021-01-29 00:26:05 -0800

    [LOWCAR] untested regressiond derivative draft
    Former-commit-id: c3e5abdcf8bf9a85644ac5246f8adbd4df5c1b19

--- lowcar/devices/KoalaBear/PID.cpp
+++ lowcar/devices/KoalaBear/PID.cpp
@@ -1,30 +1,43 @@
 #include "PID.h"
 
 #define MAX_TPS 2700.0  // maximum encoder ticks per second that a motor can go (determined empirically)
+#define SAMPLES 5		// number of samples for calculating the derivative
 
 PID::PID() {
     this->kp = this->ki = this->kd = 0.0;
-    this->prev_error = this->prev_pos = this->prev_desired_pos = 0.0;
+    this->prev_pos = this->prev_desired_pos = 0.0;
     this->integral = 0.0;
     this->velocity = 0.0;
-    this->prev_time = micros();
+	
+	this->prev_error = new float[SAMPLES];
+    this->prev_time = new unsigned long[SAMPLES];
+	for (int i = 0; i < SAMPLES; i++) {
+		this->prev_error[i] = 0.0;
+		this->prev_time[i] = micros();
+	}
 }
 
 float PID::compute(float curr_pos) {
     unsigned long curr_time = micros();                                                              // get the current time
-    float interval_secs = ((float) (curr_time - this->prev_time)) / 1000000.0;                       // compute time passed between loops, in seconds
+    float interval_secs = ((float) (curr_time - this->prev_time[SAMPLES - 1])) / 1000000.0;          // compute time passed between loops, in seconds
     float desired_pos = this->prev_desired_pos + (velocity_to_tps(this->velocity) * interval_secs);  // compute the desired position at this time
     float error = desired_pos - curr_pos;                                                            // compute the error as the set point (desired position) - process variable (current position)
     this->integral += error * interval_secs;                                                         // compute the new value of this->integral using right-rectangle approximation
+	
+	// shift the prev_error and prev_time arrays and insert curr_time and error
+	for (int i = 0; i < SAMPLES - 1; i++) {
+		this->prev_error[i] = this->prev_error[i + 1];
+		this->prev_time[i] = this->prev_time[i + 1];
+	}
+	this->prev_error[SAMPLES - 1] = error;
+	this->prev_time[SAMPLES - 1] = curr_time;
 
     // output = kp * error + ki * integral of error * kd * "derivative" of error
-    float output = (this->kp * error) + (this->ki * this->integral) + (this->kd * ((error - this->prev_error) / interval_secs));
+    float output = (this->kp * error) + (this->ki * this->integral) + (this->kd * regression(this->prev_time, this->prev_error, SAMPLES));
 
     // store the current values into previous values for use on next loop
-    this->prev_error = error;
     this->prev_pos = curr_pos;
     this->prev_desired_pos = desired_pos;
-    this->prev_time = curr_time;
 
     // if target speed is 0, output 0 automatically (prevent jittering when motor stopped) and reset integral to 0
     if (this->velocity == 0.0) {
@@ -48,7 +61,9 @@ void PID::set_velocity(float velocity) {
 // used when student sets encoder to a certain value
 void PID::set_position(float curr_pos) {
     this->prev_pos = curr_pos;
-    this->prev_error = 0.0;  // resets the error too
+	for (int i = 0; i < SAMPLES - 1; i++) {
+		this->prev_error[i] = 0.0; // resets the error too
+	}
 }
 
 // three getter functions for the three coefficients
@@ -57,6 +72,45 @@ float PID::get_ki() { return this->ki; }
 float PID::get_kd() { return this->kd; }
 
 // *********************** HELPER FUNCTIONS *********************** //
+
+// find the sum of the given floats
+float PID::sum(float nums[], int i) {
+	float total = 0.0;
+	for (int x = 0; x < i; x++) {
+		total += nums[x];
+	}
+	return total;
+}
+
+// find the average of the given floats
+float PID::average(float nums[], int i) {
+	float total = sum(nums, i);
+	return (total / (float)i);
+}
+
+// calculate the slope of least-squares linear regression through points
+// "number" is the number of elements in the arrays
+float PID::regression(unsigned long x_vals[], float y_vals[], int number) {
+    float x_avg, y_avg, slope;
+    float numerator[number], denominator[number], x_vals_float[number];
+	
+	// convert inputted x_vals to floats
+	for (int i = 0; i < number; i++) {
+		x_vals_float[i] = (float) x_vals[i];
+	}
+	
+    //calculate averages
+    x_avg = average(x_vals_float, number);
+    y_avg = average(y_vals, number); 
+
+    for (int i = 0; i < number; i++) {
+        numerator[i] = ((x_vals_float[i] - x_avg) * (y_vals[i] - y_avg));			// (x_i - x_avg) * (y_i - y_avg)
+        denominator[i] = ((x_vals_float[i] - x_avg) * (x_vals_float[i] - x_avg));	// (x_i - x_avg) ^ 2
+    }
+    slope = (sum(numerator, number) / sum(denominator, number)); 
+
+    return slope;
+}
 
 float PID::velocity_to_tps(float velocity) {
     return MAX_TPS * velocity;
