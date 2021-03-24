851eaf2e0f84f1e9d7dce116721c35adc99f7677
Vincent Le <levincent06@berkeley.edu>
2021-03-23 23:20:11 -0700

    [MISC] Implement ngrok restarting from webpage
    - ngrok_displayer is its own service.
    - Accessing "/new" endpoint will restart ngrok.service first.
    - Refreshing webpage will fetch the current IP addresses before
      displaying anything.
    
    
    Former-commit-id: c9080a8b548d6771b93d47dcd79c8cef6b1ace13

--- systemd/ngrok.sh
+++ systemd/ngrok.sh
@@ -2,14 +2,8 @@
 
 function sigint_handler {
     pkill -u pi /home/pi/ngrok
-    pkill -u pi ngrok_displayer
 }
 
 trap 'sigint_handler' INT
 
-nohup /home/pi/ngrok start --all --config /home/pi/runtime/systemd/ngrok.yml > /dev/null &
-
-sleep 5
-
-python3 ngrok_displayer.py > /dev/null
-
+nohup /home/pi/ngrok start --all --config /home/pi/runtime/systemd/ngrok.yml > /dev/null
