7cd5d5db32da2d2326a6b3446207fb1487e2fb59
Sidhant Rajadnya <srajadnya@berkeley.edu>
2022-03-09 20:32:09 -0800

    successfully implement OtherTestDevice

--- tests/integration/tc_71_0.c
+++ tests/integration/tc_71_0.c
@@ -5,7 +5,7 @@
 int main() {
     start_test("Other Device Test", "", NO_REGEX);
 
-    char* dev_name = "SimpleTestDevice";
+    char* dev_name = "OtherTestDevice";
     int socket = connect_virtual_device(dev_name, UID);
     sleep(1);
 
@@ -16,17 +16,19 @@ int main() {
     sleep(1);
     check_device_not_connected(UID);
 
-    // connect_virtual_device(dev_name, UID);
-    // sleep(1);
+    connect_virtual_device(dev_name, UID);
+    sleep(1);
+    check_device_connected(UID);
 
-    // uint8_t dev_type = device_name_to_type(dev_name);
-    // device_t* dev = get_device(dev_type);
+    uint8_t dev_type = device_name_to_type(dev_name);
+    device_t* dev = get_device(dev_type);
 
     // param_val_t device_values[dev->num_params];
 
-    // param_val_t constant_value = {.p_i = 100};
 
-    // same_param_value(dev_name, UID, "CONSTANT", INT, constant_value);
+    param_val_t constant_value = {.p_i = 100};
+
+    same_param_value(dev_name, UID, "CONSTANT", INT, constant_value);
 
     return 0;
 }
