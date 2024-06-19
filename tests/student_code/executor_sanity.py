58d70f0ee681d3250831b8edc3d31c36821d4b5d
ishan-monie <ishanmonie@berkeley.edu>
2024-06-18 22:36:41 -0500

    modified first 5 student_code tests

--- tests/student_code/executor_sanity.py
+++ tests/student_code/executor_sanity.py
@@ -1,3 +1,4 @@
+'''
 import time
 import math
 
@@ -23,3 +24,26 @@ def teleop_setup():
 
 def teleop_main():
 	oops = 1 / 0
+'''
+
+import time
+import math
+
+# This autonomous code prints some output every two seconds
+
+def constant_print(msg):
+    while True:
+        print(f"{msg} printing again")
+        time.sleep(2)
+
+def autonomous():
+    print('Autonomous has begun!')
+    print(f"Starting position: {Robot.start_pos}")
+    while True:
+        Robot.run(constant_print, "autonomous")
+
+# This teleop code generates a DivisionByZero Python error
+
+def teleop():
+    while True:
+        oops = 1 / 0
