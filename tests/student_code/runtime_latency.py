d7fb74a5b20181539f74c4a6c7f1ecd8c9f3c952
Luke Wang <56856692+3142008956@users.noreply.github.com>
2024-06-18 21:24:53 -0700

    Revert "modified first 5 student_code tests"

--- tests/student_code/runtime_latency.py
+++ tests/student_code/runtime_latency.py
@@ -2,10 +2,15 @@ import time
 
 time_dev = '60_123'
 
-def autonomous():
+def autonomous_setup():
     pass
 
-def teleop():
-    while True:
-        if Gamepad.get_value('button_a'):
-            Robot.set_value(time_dev, "GET_TIME", True)
+def autonomous_main():
+    pass
+
+def teleop_setup():
+    pass
+
+def teleop_main():
+    if Gamepad.get_value('button_a'):
+        Robot.set_value(time_dev, "GET_TIME", True)
