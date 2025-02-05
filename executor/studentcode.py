c3009166671c43c8b103761eeb15535bee220b04
Luke Wang <lukewang854@berkeley.com>
2025-02-04 21:04:34 -0800

    Created new branch for executor_rewrite for Luke

--- executor/studentcode.py
+++ executor/studentcode.py
@@ -1,5 +1,19 @@
+import time
+import math
+
+# This autonomous code prints some output every two seconds
+
+def constant_print(msg):
+	while True:
+		print(f"{msg} printing again")
+		time.sleep(2)
+
 def autonomous():
-    print("Autonomous has begun!")
+	print('Autonomous setup has begun!')
+	print(f"Starting position: {Robot.start_pos}")
+	Robot.run(constant_print, "autonomous")
+
+# This teleop code generates a DivisionByZero Python error
 
 def teleop():
-    print("Teleop has begun!")
+	oops = 1 / 0
