fe05c5d82b3763afe9a0c4dd0711d83432f87ede
Brandon Wong <brandonwong14@berkeley.edu>
2025-03-05 20:28:59 -0800

    [TESTS] updated tc_71_2 python error message for version 3.12, forgot a newline

--- tests/integration/tc_71_2.c
+++ tests/integration/tc_71_2.c
@@ -11,7 +11,7 @@ char check_output_6[] =
     "Traceback (most recent call last):\n"
     "    File \"/home/runner/work/runtime/runtime/tests/student_code/executor_sanity.py\", line 25, in teleop_main\n"
     "        oops = 1 / 0\n"
-    "               ~~^~~"
+    "               ~~^~~\n"
     "ZeroDivisionError: division by zero\n";
 
 int main() {
